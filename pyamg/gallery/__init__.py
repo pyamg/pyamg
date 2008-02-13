0af12d153d106dc6d5bbe7f7a385b4ce95e65cfd
Nathan Bell <wnbell@gmail.com>
2008-02-13 03:59:08 +0000

    added support for loading examples from disk

--- pyamg/gallery/__init__.py
+++ pyamg/gallery/__init__.py
@@ -4,6 +4,7 @@ from info import __doc__
 
 from laplacian import *
 from elasticity import *
+from example import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 from scipy.testing.pkgtester import Tester
---
+++ pyamg/gallery/example.py
@@ -0,0 +1,69 @@
+"""Examples stored in files"""
+
+import os
+from fnmatch import fnmatch
+from glob import glob
+from itertools import izip
+
+from scipy.io import loadmat
+
+__all__ = ['load_example']
+
+### recursive attempt
+##def rec_glob(base,pattern):
+##    matched_files = []
+##
+##    for dirpath, dirnames, filenames in os.walk(base):
+##        print "dirpath",dirpath
+##        for filename in filenames:
+##            if fnmatch(filename, pattern):
+##                matched_files.append( os.path.join( dirpath, filename) )
+##
+##    return matched_files
+##
+##base_dir = os.path.split(__file__)[0]
+##example_dir = os.path.join( base_dir, 'example_data')
+##example_files = rec_glob(example_dir,'*.mat')
+##example_files = [ os.path.join( example_dir, fn ) for fn in example_files ]
+##example_names = [ os.path.split(name)[1][:-4] for name in example_files ]
+##examples = dict( izip(example_names,example_files) )
+##example_names.sort()
+
+
+base_dir = os.path.split(__file__)[0]
+example_dir = os.path.join( base_dir, 'example_data')
+example_files = glob( os.path.join(example_dir, '*.mat') )
+example_names = [ os.path.split(name)[1][:-4] for name in example_files ]
+
+example_names.sort()
+
+def load_example(name):
+    """Load an example problem by name
+       
+    Each example is stored in a dictionary with one
+    or more of the following keys:
+        'A'        : sparse matrix
+        'B'        : near-nullspace candidates
+        'vertices' : dense array of nodal coordinates
+        'elements' : dense array of element indices
+
+    Current example names are:%s
+
+    Examples
+    ========
+    >>> from pyamg.gallery import load_example
+    >>> ex = load_example('knot')
+
+    """ 
+    
+    if name not in example_names:
+        raise ValueError('no example with name (%s)' % name)
+    else:
+        return loadmat( os.path.join(example_dir, name + '.mat') )
+   
+# insert the example names into the docstring
+load_example.__doc__ %= ('\n' + ' ' * 8).join( [''] + example_names)
+
+
+
+
