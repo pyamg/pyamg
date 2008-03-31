e3d74ff455a7f554994510e80f06cdee0e8ca431
Luke Olson <luke.olson@gmail.com>
2008-03-31 18:00:50 +0000

    touching options class

---
+++ pyamg/mgopts.py
@@ -0,0 +1,15 @@
+"""Options class for multigrid solvers and cycling.  Includes defaults and
+checking for various parameters"""
+
+class mgopts:
+    def __init__(self,mgtype):
+        # set up the dictionary
+
+    def __repr__(self):
+        # print the dictionary
+
+    def check_opts(self):
+        # run checks on the options
+
+    def auto_gen(self):
+        # attempt to initialize or reanitialize the options automatically
