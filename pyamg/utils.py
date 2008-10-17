e654c03141cb6d8ae2effd8daab353ab9aaaff34
Luke Olson <luke.olson@gmail.com>
2008-10-17 21:00:54 +0000

    updated doc strings.  added a some checking

--- pyamg/utils.py
+++ pyamg/utils.py
@@ -1,27 +1,76 @@
-__all__ = ['approximate_spectral_radius', 'infinity_norm', 'diag_sparse',
-        'norm', 'profile_solver']
-__all__ += ['UnAmal', 'Coord2RBM', 'BSR_Get_Row', 'BSR_Row_WriteScalar', 
-        'BSR_Row_WriteVect' ]
+"""General utility functions for pyamg"""
 
-import numpy
-import scipy
+__docformat__ = "restructuredtext en"
+
 from numpy import fromfile, ascontiguousarray, mat, int32, inner, dot, \
                   ravel, arange, concatenate, tile, asarray, sqrt, diff, \
-                  zeros, ones, empty, asmatrix, array
+                  zeros, ones, empty, asmatrix, array, random, rank
 from scipy import rand, real                  
-from scipy.linalg import eigvals
+from scipy.linalg import eigvals, norm
 from scipy.lib.blas import get_blas_funcs
 from scipy.sparse import isspmatrix, isspmatrix_csr, isspmatrix_csc, \
         isspmatrix_bsr, csr_matrix, csc_matrix, bsr_matrix, coo_matrix
 from scipy.sparse.sputils import upcast
 from scipy.sparse.linalg import eigen, eigen_symmetric
 
+__all__ = ['approximate_spectral_radius', 'infinity_norm', 'diag_sparse',
+        'norm', 'profile_solver']
+__all__ += ['UnAmal', 'Coord2RBM', 'BSR_Get_Row', 'BSR_Row_WriteScalar', 
+        'BSR_Row_WriteVect' ]
+
+
 def norm(x):
-    #currently 40x faster than scipy.linalg.norm(x)
+    """
+    2-norm of a vector
+    
+    Parameters
+    ----------
+    x : array_like
+        Vector of complex or real values
+
+    Return
+    ------
+    n : float
+        2-norm of a vector
+
+    Notes
+    -----
+    - currently 40x faster than scipy.linalg.norm(x), which calls
+      sqrt(numpy.sum(real((conjugate(x)*x)),axis=0)) resulting in an extra copy
+    - only handles the 2-norm for vectors
+
+    See Also
+    --------
+    scipy.linalg.norm : scipy general matrix or vector norm
+    """
+
     x = ravel(x)
     return real(sqrt(inner(x,x)))
 
 def axpy(x,y,a=1.0):
+    """
+    Quick level-1 call to blas::
+    y = a*x+y
+
+    Parameters
+    ----------
+    x : array_like
+        nx1 real or complex vector
+    y : array_like
+        nx1 real or complex vector
+    a : float
+        real or complex scalar
+
+    Return
+    ------
+    y : array_like
+        Input variable y is rewritten
+
+    Notes
+    -----
+    The call to get_blas_funcs automatically determines the prefix for the blas
+    call.
+    """
     fn = get_blas_funcs(['axpy'], [x,y])[0]
     fn(x,y,a)
 
@@ -55,7 +104,8 @@ def axpy(x,y,a=1.0):
 
 
 def approximate_spectral_radius(A,tol=0.1,maxiter=10,symmetric=None):
-    """approximate the spectral radius of a matrix
+    """
+    Approximate the spectral radius of a matrix
 
     Parameters
     ----------
@@ -74,7 +124,35 @@ def approximate_spectral_radius(A,tol=0.1,maxiter=10,symmetric=None):
 
     Returns
     -------
-        An approximation to the spectral radius of A
+    An approximation to the spectral radius of A
+
+    Notes
+    -----
+    The spectral radius is approximated by looking at the Ritz eigenvalues.
+    Arnoldi iteration (or Lanczos) is used to project the matrix A onto a
+    Krylov subspace: H = Q* A Q.  The eigenvalues of H (i.e. the Ritz
+    eigenvalues) should represent the eigenvalues of A in the sense that the
+    minimum and maximum values are usually well matched (for the symmetric case
+    it is true since the eigenvalues are real).
+
+    References
+    ----------
+    Z. Bai, J. Demmel, J. Dongarra, A. Ruhe, and H. van der Vorst, editors.
+    "Templates for the Solution of Algebraic Eigenvalue Problems: A Practical
+    Guide", SIAM, Philadelphia, 2000.
+
+    Examples
+    --------
+    >>> from pyamg.utils import approximate_spectral_radius
+    >>> from scipy import rand
+    >>> from scipy.linalg import eigvals, norm
+    >>> A = rand(10,10)
+    >>> print approximate_spectral_radius(A,maxiter=3)
+    >>> print max([norm(x) for x in eigvals(A)])
+
+    TODO
+    ----
+    Make the method adaptive (restarts)
     """
    
     if type(A) == type( array([0.0]) ):
@@ -85,9 +163,7 @@ def approximate_spectral_radius(A,tol=0.1,maxiter=10,symmetric=None):
 
     maxiter = min(A.shape[0],maxiter)
 
-    #TODO make method adaptive
-
-    numpy.random.seed(0)  #make results deterministic
+    random.seed(0)  #make results deterministic
 
     v0  = rand(A.shape[1],1)
     v0 /= norm(v0)
@@ -145,6 +221,41 @@ def approximate_spectral_radius(A,tol=0.1,maxiter=10,symmetric=None):
 
 
 def profile_solver(ml, accel=None, **kwargs):
+    """
+    A quick solver to profile a particular multilevel object
+
+    Parameters
+    ----------
+    ml : multilevel
+        Fully constructed multilevel object
+    accel : function pointer
+        Pointer to a valid Krylov solver (e.g. gmres, cg)
+
+    Returns
+    -------
+    residuals : array
+        Array of residuals for each iteration
+
+    See Also
+    --------
+    multilevel.psolve, multilevel.solve
+
+    Examples
+    --------
+    >>> from numpy import ones
+    >>> from scipy.sparse import spdiags
+    >>> from scipy.sparse.linalg import cg
+    >>> from pyamg.classical import ruge_stuben_solver
+    >>> from pyamg.utils import profile_solver
+    >>> n=100
+    >>> e = ones((n,1)).ravel()
+    >>> data = [ -1*e, 2*e, -1*e ]
+    >>> A = spdiags(data,[-1,0,1],n,n)
+    >>> b = A*ones(A.shape[0])
+    >>> ml = ruge_stuben_solver(A, max_coarse=10)
+    >>> res = profile_solver(ml,accel=cg)
+    >>> print res
+    """
     A = ml.levels[0].A
     b = A * rand(A.shape[0],1)
 
@@ -162,16 +273,48 @@ def profile_solver(ml, accel=None, **kwargs):
 
 def infinity_norm(A):
     """
-    Infinity norm of a sparse matrix (maximum absolute row sum).  This serves
-    as an upper bound on spectral radius.
+    Infinity norm of a matrix (maximum absolute row sum).  
+
+    Parameters
+    ----------
+    A : csr_matrix, csc_matrix, sparse, or numpy matrix
+        Sparse or dense matrix
+    
+    Returns
+    -------
+    n : float
+        Infinity norm of the matrix
+    
+    Notes
+    -----
+    - This serves as an upper bound on spectral radius.
+    - csr and csc avoid a deep copy
+    - dense calls scipy.linalg.norm
+
+    See Also
+    --------
+    scipy.linalg.norm : dense matrix norms
+
+    Examples
+    --------
+    >>> from numpy import ones
+    >>> from scipy.sparse import spdiags
+    >>> from pyamg.utils import infinity_norm
+    >>> n=100
+    >>> e = ones((n,1)).ravel()
+    >>> data = [ -1*e, 2*e, -1*e ]
+    >>> A = spdiags(data,[-1,0,1],n,n)
+    >>> print infinity_norm(A)
     """
 
     if isspmatrix_csr(A) or isspmatrix_csc(A):
         #avoid copying index and ptr arrays
         abs_A = A.__class__((abs(A.data),A.indices,A.indptr),shape=A.shape)
-        return (abs_A * ones(A.shape[1],dtype=A.dtype)).max()
+        return (abs_A * ones((A.shape[1]),dtype=A.dtype)).max()
+    elif isspmatrix(A):
+        return (abs(A) * ones((A.shape[1]),dtype=A.dtype)).max()
     else:
-        return (abs(A) * ones(A.shape[1],dtype=A.dtype)).max()
+        return norm(A,inf)
 
 def diag_sparse(A):
     """
@@ -180,15 +323,70 @@ def diag_sparse(A):
 
     Otherwise
        - return a csr_matrix with A on the diagonal
-    """
 
-    #TODO integrate into SciPy?
+    Parameters
+    ----------
+    A : sparse matrix or rank 1 array
+        General sparse matrix or array of diagonal entries
+
+    Returns
+    -------
+    B : array or sparse matrix
+        Diagonal sparse is returned as csr if A is dense otherwise return an
+        array of the diagonal
+
+    Examples
+    --------
+    >>> from scipy import rand
+    >>> from pyamg.utils import diag_sparse
+    >>> d = rand(6,1).ravel()
+    >>> print diag_sparse(d).todense()
+    """
     if isspmatrix(A):
         return A.diagonal()
     else:
+        if(rank(A)!=1):
+            raise ValueError,'input diagonal array expected to be rank 1'
         return csr_matrix((asarray(A),arange(len(A)),arange(len(A)+1)),(len(A),len(A)))
 
 def scale_rows(A,v,copy=True):
+    """
+    Scale the sparse rows of a matrix
+
+    Parameters
+    ----------
+    A : sparse matrix
+        Sparse matrix with M rows
+    v : array_like
+        Array of M scales
+    copy : {True,False}
+        TODO
+
+    Returns
+    -------
+    A : sparse matrix
+        Scaled sparse matrix in original format
+
+    See Also
+    --------
+    scipy.sparse.sparsetools.csr_scale_rows, scale_columns
+
+    Notes
+    -----
+    - if A is a csc_matrix, the transpose A.T is passed to scale_columns
+    - if A is not csr, csc, or bsr, it is converted to csr and sent to scale_rows
+
+    Examples
+    --------
+    >>> from numpy import ones
+    >>> from scipy.sparse import spdiags
+    >>> from pyamg.utils import scale_rows
+    >>> n=5
+    >>> e = ones((n,1)).ravel()
+    >>> data = [ -1*e, 2*e, -1*e ]
+    >>> A = spdiags(data,[-1,0,1],n,n-1).tocsr()
+    >>> print scale_rows(A,5*ones((A.shape[0],1))).todense()
+    """
     from scipy.sparse.sparsetools import csr_scale_rows, bsr_scale_rows
 
     v = ravel(v)
@@ -217,6 +415,43 @@ def scale_rows(A,v,copy=True):
         return scale_rows(csr_matrix(A),v)
         
 def scale_columns(A,v,copy=True):
+    """
+    Scale the sparse columns of a matrix
+
+    Parameters
+    ----------
+    A : sparse matrix
+        Sparse matrix with N rows
+    v : array_like
+        Array of N scales
+    copy : {True,False}
+        TODO
+
+    Returns
+    -------
+    A : sparse matrix
+        Scaled sparse matrix in original format
+
+    See Also
+    --------
+    scipy.sparse.sparsetools.csr_scale_columns, scale_rows
+
+    Notes
+    -----
+    - if A is a csc_matrix, the transpose A.T is passed to scale_rows
+    - if A is not csr, csc, or bsr, it is converted to csr and sent to scale_rows
+
+    Examples
+    --------
+    >>> from numpy import ones
+    >>> from scipy.sparse import spdiags
+    >>> from pyamg.utils import scale_columns
+    >>> n=5
+    >>> e = ones((n,1)).ravel()
+    >>> data = [ -1*e, 2*e, -1*e ]
+    >>> A = spdiags(data,[-1,0,1],n,n-1).tocsr()
+    >>> print scale_columns(A,5*ones((A.shape[1],1))).todense()
+    """
     from scipy.sparse.sparsetools import csr_scale_columns, bsr_scale_columns
 
     v = ravel(v)
@@ -245,6 +480,48 @@ def scale_columns(A,v,copy=True):
         return scale_rows(csr_matrix(A),v)
 
 def symmetric_rescaling(A,copy=True):
+    """
+    Scale the matrix symmetrically::
+
+    A = D^{-1/2} A D^{-1/2}
+
+    where D=diag(A).
+
+    The left multiplication is accomplished through scale_rows and the right
+    multiplication is done through scale columns.
+
+    Parameters
+    ----------
+    A : sparse matrix
+        Sparse matrix with N rows
+    copy : {True,False}
+        TODO
+
+    Returns
+    -------
+    D_sqrt : array
+        Array of sqrt(diag(A))
+    D_sqrt_inv : array
+        Array of 1/sqrt(diag(A))
+    DAD    : csr_matrix
+        Symmetrically scaled A
+
+    Notes
+    -----
+    - if A is not csr, it is converted to csr and sent to scale_rows
+
+    Examples
+    --------
+    >>> from numpy import ones
+    >>> from scipy.sparse import spdiags
+    >>> from pyamg.utils import symmetric_rescaling
+    >>> n=5
+    >>> e = ones((n,1)).ravel()
+    >>> data = [ -1*e, 2*e, -1*e ]
+    >>> A = spdiags(data,[-1,0,1],n,n).tocsr()
+    >>> Ds, Dsi, DAD = symmetric_rescaling(A)
+    >>> print DAD.todense()
+    """
     if isspmatrix_csr(A) or isspmatrix_csc(A) or isspmatrix_bsr(A):
         if A.shape[0] != A.shape[1]:
             raise ValueError,'expected square matrix'
@@ -298,7 +575,9 @@ def symmetric_rescaling(A,copy=True):
 ##############################################################################################
 
 def UnAmal(A, RowsPerBlock, ColsPerBlock):
-    """Unamalgamate a CSR A with blocks of 1's.  
+    """
+    Unamalgamate a CSR A with blocks of 1's.  
+
     Equivalent to Kronecker_Product(A, ones(RowsPerBlock, ColsPerBlock)
 
     Input
