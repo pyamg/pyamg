76f459b4c8dad299271709cd2cc0c86ab76e7a33
Luke Olson <luke.olson@gmail.com>
2008-11-15 00:00:12 +0000

    updated docstrings and example

--- pyamg/aggregation/aggregate.py
+++ pyamg/aggregation/aggregate.py
@@ -26,9 +26,9 @@ def standard_aggregation(C):
 
     Examples
     --------
-
     >>> from scipy.sparse import csr_matrix
     >>> from pyamg import poisson
+    >>> from pyamg.aggregation.aggregate import standard_aggregation
     >>> A = poisson((4,), format='csr')   # 1D mesh with 4 vertices
     >>> A.todense()
     matrix([[ 2., -1.,  0.,  0.],
@@ -49,6 +49,10 @@ def standard_aggregation(C):
     matrix([[0],
             [1],
             [1]], dtype=int8)
+
+    See Also
+    --------
+    multigridtools.standard_aggregation
 
     """
 
@@ -96,12 +100,17 @@ def lloyd_aggregation(C, ratio=0.03, distance='unit', maxiter=10):
         Fraction of the nodes which will be seeds.
     distance : ['unit','abs','inv',None]
         Distance assigned to each edge of the graph G used in Lloyd clustering
+
         For each nonzero value C[i,j]:
-            'unit' : G[i,j] = 1 
-            'abs'  : G[i,j] = abs(C[i,j])
-            'inv'  : G[i,j] = 1.0/abs(C[i,j])
-            'same' : G[i,j] = C[i,j]
-            'sub'  : G[i,j] = C[i,j] - min(C)
+
+        =======  ===========================
+        'unit'   G[i,j] = 1 
+        'abs'    G[i,j] = abs(C[i,j])
+        'inv'    G[i,j] = 1.0/abs(C[i,j])
+        'same'   G[i,j] = C[i,j]
+        'sub'    G[i,j] = C[i,j] - min(C)
+        =======  ===========================
+
     maxiter : int
         Maximum number of iterations to perform
 
@@ -113,7 +122,29 @@ def lloyd_aggregation(C, ratio=0.03, distance='unit', maxiter=10):
 
     Examples
     --------
+    >>> from scipy.sparse import csr_matrix
+    >>> from pyamg import poisson
+    >>> from pyamg.aggregation.aggregate import lloyd_aggregation
+    >>> A = poisson((4,), format='csr')   # 1D mesh with 4 vertices
+    >>> A.todense()
+    matrix([[ 2., -1.,  0.,  0.],
+            [-1.,  2., -1.,  0.],
+            [ 0., -1.,  2., -1.],
+            [ 0.,  0., -1.,  2.]])
+    >>> lloyd_aggregation(A).todense() # one aggregate
+    matrix([[1],
+            [1],
+            [1],
+            [1]], dtype=int8)
+    >>> lloyd_aggregation(A,ratio=0.5).todense() # more seeding for two aggregates
+    matrix([[0, 1],
+            [0, 1],
+            [1, 0],
+            [1, 0]], dtype=int8)
 
+    See Also
+    --------
+    multigridtools.standard_aggregation
 
     """
 
@@ -149,13 +180,3 @@ def lloyd_aggregation(C, ratio=0.03, distance='unit', maxiter=10):
     data = ones(len(row), dtype='int8')
     AggOp = coo_matrix( (data,(row,col)), shape=(G.shape[0],num_seeds)).tocsr()
     return AggOp
-
-
-
-
-
-
-#from pyamg.utils import dispatcher
-#name_to_handle = dict([ (fn[:-len('_aggregation')], eval(fn)) for fn in __all__]) 
-#dispatch = dispatcher( name_to_handle )
-
