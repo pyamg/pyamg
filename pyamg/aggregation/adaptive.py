66b31a84c81df0d5cf70aaaa5f9991d5f14b8aba
Jacob Schroder <jacob.bb.schroder@gmail.com>
2008-11-24 18:48:37 +0000

    Fixing spurious update to adaptive

--- pyamg/aggregation/adaptive.py
+++ pyamg/aggregation/adaptive.py
@@ -268,21 +268,9 @@ def initial_setup_stage(A, mat_flag, pdef, candidate_iters, epsilon, max_levels,
                 if x_A_x == 0:
                     x = x_hat  #need to restore x
 
-    import pylab; from scipy import linspace, real, imag; iter = len(Ps)*2
     # extend coarse-level candidate to the finest level
     for A_l,P in reversed(zip(As[1:],Ps)):
         relax(A_l,x)
-        
-        pylab.figure(100+iter)
-        pylab.plot(linspace(0.0, 1.0, x.shape[0]), real(x), '-ro')
-        handle = pylab.title('Level %d Real(Initial Candidate)'%iter)
-        handle.set_fontsize(18)
-        pylab.figure(100+iter+1)
-        pylab.plot(linspace(0.0, 1.0, x.shape[0]), imag(x), '-ro')
-        handle = pylab.title('Level %d Imag(Initial Candidate)'%iter)
-        handle.set_fontsize(18)
-        iter -= 2
-        
         x = P * x
     relax(A,x)
 
