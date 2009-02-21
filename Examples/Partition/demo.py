828a35c461e3af7781301d1ce5783213f98b7827
Luke Olson <luke.olson@gmail.com>
2009-02-21 20:27:43 +0000

    easier to read demo

--- Examples/Partition/demo.py
+++ Examples/Partition/demo.py
@@ -1,41 +1,12 @@
-def trimesh(vertices, indices, labels=False):
-    """
-    Plot a 2D triangle mesh
-    """
-    from scipy import asarray
-    from matplotlib import collections
-    from pylab import gca, axis, text
-    from numpy import average
-    
-    vertices,indices = asarray(vertices),asarray(indices)
-
-    #3d tensor [triangle index][vertex index][x/y value]
-    triangles = vertices[indices.ravel(),:].reshape((indices.shape[0],3,2))
-    
-    col = collections.PolyCollection(triangles)
-    col.set_facecolor('grey')
-    col.set_alpha(0.5)
-    col.set_linewidth(1)
-
-    sub = gca()
-    sub.add_collection(col,autolim=True)
-    axis('off')
-    sub.autoscale_view()
-
-    if labels:
-        barycenters = average(triangles,axis=1)
-        for n,bc in enumerate(barycenters):
-            text(bc[0], bc[1], str(n), {'color' : 'k', 'fontsize' : 8,
-                                        'horizontalalignment' : 'center',
-                                        'verticalalignment' : 'center'})
-
-########################################################################
-from numpy import kron, ones, array, zeros, median, where
+from numpy import ones, zeros, median, where
 from scipy import rand
-from scipy.sparse import csr_matrix, coo_matrix
-from scipy.sparse.linalg import lobpcg, eigen
+from scipy.sparse.linalg import lobpcg
+
 from pylab import gca, show, figure
+
 from pyamg import smoothed_aggregation_solver
+
+from helper import trimesh, graph_laplacian
 
 meshnum = 2
 
@@ -44,22 +15,11 @@ if meshnum==1:
     V,E = mesh.uniform_tri(20,6)
 if meshnum==2:
     from scipy.io import loadmat
-    mesh = loadmat('crack_mesh.mat')
+    mesh = loadmat('crack_mesh_2.mat')
     V=mesh['V']
     E=mesh['E']
 
-# build graph laplacian
-Nel = E.shape[0]
-Npts = E.max()+1
-row = kron(range(0,Nel),[1,1,1])
-col = E.ravel()
-data = ones((col.size,),dtype=float)
-A = coo_matrix((data,(row,col)), shape=(Nel,Npts)).tocsr()
-A = A.T * A
-A.data = -1*ones((A.nnz,),dtype=float)
-A.setdiag(zeros((Npts,),dtype=float))
-A.setdiag(-1*array(A.sum(axis=1)).ravel())
-A = A.tocsr()
+A = graph_laplacian(V,E)
 
 # construct preconditioner
 ml = smoothed_aggregation_solver(A, coarse_solver='pinv2',max_coarse=10)
@@ -68,14 +28,16 @@ M = ml.aspreconditioner()
 # solve for lowest two modes: constant vector and Fiedler vector
 X = rand(A.shape[0], 2) 
 (eval,evec,res) = lobpcg(A, X, M=M, tol=1e-12, largest=False, \
-        retResidualNormsHistory=True)
+        verbosityLevel=1, retResidualNormsHistory=True)
+
+fiedler = evec[:,1]
 
 # use the median of the Fiedler vector as a the separator
-vmed = median(evec[:,1])
-v = zeros((Npts,))
-K = where(evec[:,1]>vmed)[0]
+vmed = median(fiedler)
+v = zeros((A.shape[0],))
+K = where(fiedler>vmed)[0]
 v[K]=-1
-K = where(evec[:,1]>vmed)[0]
+K = where(fiedler>vmed)[0]
 v[K]=1
 
 # plot the mesh and partition
@@ -83,5 +45,5 @@ trimesh(V,E)
 sub = gca()
 sub.hold(True)
 sub.scatter(V[:,0],V[:,1],marker='o',s=50,c=v)
-#sub.scatter(V[:,0],V[:,1],marker='o',s=50,c=evec[:,1])
+#sub.scatter(V[:,0],V[:,1],marker='o',s=50,c=fiedler)
 show()
---
+++ Examples/Partition/helper.py
@@ -0,0 +1,46 @@
+def trimesh(vertices, indices, labels=False):
+    """
+    Plot a 2D triangle mesh
+    """
+    from scipy import asarray
+    from matplotlib import collections
+    from pylab import gca, axis, text
+    from numpy import average
+    
+    vertices,indices = asarray(vertices),asarray(indices)
+
+    #3d tensor [triangle index][vertex index][x/y value]
+    triangles = vertices[indices.ravel(),:].reshape((indices.shape[0],3,2))
+    
+    col = collections.PolyCollection(triangles)
+    col.set_facecolor('grey')
+    col.set_alpha(0.5)
+    col.set_linewidth(1)
+
+    sub = gca()
+    sub.add_collection(col,autolim=True)
+    axis('off')
+    sub.autoscale_view()
+
+    if labels:
+        barycenters = average(triangles,axis=1)
+        for n,bc in enumerate(barycenters):
+            text(bc[0], bc[1], str(n), {'color' : 'k', 'fontsize' : 8,
+                                        'horizontalalignment' : 'center',
+                                        'verticalalignment' : 'center'})
+
+def graph_laplacian(V,E):
+    # build graph laplacian
+    from numpy import kron, ones, zeros, array
+    from scipy.sparse import coo_matrix
+    Nel = E.shape[0]
+    Npts = E.max()+1
+    row = kron(range(0,Nel),[1,1,1])
+    col = E.ravel()
+    data = ones((col.size,),dtype=float)
+    A = coo_matrix((data,(row,col)), shape=(Nel,Npts)).tocsr()
+    A = A.T * A
+    A.data = -1*ones((A.nnz,),dtype=float)
+    A.setdiag(zeros((Npts,),dtype=float))
+    A.setdiag(-1*array(A.sum(axis=1)).ravel())
+    return A.tocsr()
