3bc188f8845d0448f668d1e8a7a7a368bbe56318
Luke Olson <luke.olson@gmail.com>
2009-02-22 19:19:35 +0000

    added an example of an interface with FiPy

---
+++ Examples/FiPyFormulation/PyAMGSolver.py
@@ -0,0 +1,67 @@
+__docformat__ = 'restructuredtext'
+
+import sys
+from pysparse import precon
+from pysparse import itsolvers
+from fipy.solvers.pysparse.pysparseSolver import PysparseSolver
+from fipy.tools.pysparseMatrix import _PysparseMatrix
+from fipy.solvers.solver import Solver
+
+from numpy import array, copy
+from scipy.sparse import csr_matrix
+from pyamg import smoothed_aggregation_solver
+
+class PyAMGSolver(Solver):
+    """
+    The PyAMGSolver class.
+
+    def __init__(self, tolerance=1e-10, iterations=1000, precon=None):
+    """
+    def __init__(self, *args, **kwargs):
+        if kwargs.has_key('verbosity'):
+            self.verbosity = kwargs['verbosity']
+        else:
+            self.verbosity = False
+
+        if kwargs.has_key('MGSetupOpts'):
+            self.MGSetupOpts = kwargs['MGSetupOpts']
+        else:
+            self.MGSetupOpts = {}
+
+        if kwargs.has_key('MGSolveOpts'):
+            self.MGSolveOpts = kwargs['MGSolveOpts']
+        else:
+            self.MGSolveOpts = {}
+
+    def _getMatrixClass(self):
+        return _PysparseMatrix
+     
+    def _solve(self, L, x, b):
+        relres=[]
+        
+        # create scipy.sparse matrix view
+        (data,row,col) = L._getMatrix().find()
+        A = csr_matrix((data,(row,col)),shape=L._getMatrix().shape)
+
+        # solve and deep copy data
+        ml = smoothed_aggregation_solver(A,**self.MGSetupOpts)
+        xx = ml.solve(b=b,residuals=relres,**self.MGSolveOpts)
+        for i in range(0,len(xx)):
+            x[i]=xx[i]
+
+        # fix relres and set info
+        if len(relres)>0:
+            relres=array(relres)/relres[0]
+            info = 0
+        iter = len(relres)
+
+        if self.verbosity:
+            print ml
+            print 'MG iterations: %d'%iter
+            print 'MG convergence factor: %g'%((relres[-1])**(1.0/iter))
+            print 'MG residual history: ', relres
+
+        self._raiseWarning(info, iter, relres)
+            
+    def _canSolveAssymetric(self):
+        return False
---
+++ Examples/FiPyFormulation/demo.py
@@ -0,0 +1,38 @@
+# This example solves a diffusion problem and demonstrates the use of
+# applying boundary condition patches.
+from fipy import *
+from PyAMGSolver import PyAMGSolver
+#
+nx = 20
+ny = nx
+dx = 1.0
+dy = dx
+L = dx * nx
+mesh = Grid2D(dx=dx, dy=dy, nx=nx, ny=ny)
+phi = CellVariable(name = "solution variable", mesh = mesh, value = 0.)
+
+# Apply Dirichlet boundary conditions (else Neumann by defualt)
+valueTopLeft = 0
+valueBottomRight = 1
+x, y = mesh.getFaceCenters()
+facesTopLeft = ((mesh.getFacesLeft() & (y > L / 2))
+                | (mesh.getFacesTop() & (x < L / 2)))
+facesBottomRight = ((mesh.getFacesRight() & (y < L / 2))
+                    | (mesh.getFacesBottom() & (x > L / 2)))
+BCs = (FixedValue(faces=facesTopLeft, value=valueTopLeft),
+       FixedValue(faces=facesBottomRight, value=valueBottomRight))
+
+# set solver
+#solver = LinearLUSolver(tolerance = 1.e-6, iterations = 100)
+MGSetupOpts = {'max_coarse':10}
+MGSolveOpts = {'maxiter':100, 'tol':1e-8}
+solver = PyAMGSolver(verbosity=1,MGSetupOpts=MGSetupOpts,MGSolveOpts=MGSolveOpts)
+
+# solve diffusion equation with solver above
+ImplicitDiffusionTerm().solve(var=phi, 
+                              boundaryConditions = BCs,
+                              solver = solver)
+
+# view result
+viewer = Viewer(vars=phi, datamin=0., datamax=1.)
+viewer.plot()
