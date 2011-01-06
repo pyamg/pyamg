c2f6051af69fe511ea76bcf5e40520306b46daf3
Luke Olson <luke.olson@gmail.com>
2011-01-06 22:36:55 +0000

    fixed the Dolfin example

--- Examples/DolfinFormulation/demo.py
+++ Examples/DolfinFormulation/demo.py
@@ -1,53 +1,62 @@
-# This example uses Dolfin of the Fenics Project:
-# http://www.fenics.org/wiki/DOLFIN
+# This example uses Dolfin version 0.9.7 of the Fenics Project:
+# http://www.fenicsproject.org/
 # to construct a FE solution the Poisson problem on the Unit Square.
 #
 # PyAMG is used to solve the resulting system.  Data is not copied when
 # constructing the scipy.sparse matrix
-#
-from dolfin import *
 
-from scipy.sparse import csr_matrix
-from pyamg import smoothed_aggregation_solver
-import pylab
 
 ############################################################
-# Dolfin
-dolfin_set("linear algebra backend","uBLAS")
+# Part I: Setup problem with Dolfin
+from dolfin import *
+parameters.linear_algebra_backend = "uBLAS"
 
-mesh = UnitSquare(75, 75)
+# Define mesh, function space
+mesh = UnitSquare(75, 35)
 V = FunctionSpace(mesh, "CG", 1)
+
+# Define basis and bilinear form
+u = TrialFunction(V)
+v = TestFunction(V)
+a = dot(grad(v), grad(u))*dx
+f = Expression('500.0 * exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)')
+L = v*f*dx
 
 # Define Dirichlet boundary (x = 0 or x = 1)
 class DirichletBoundary(SubDomain):
     def inside(self, x, on_boundary):
         return x[0] < DOLFIN_EPS or x[0] > 1.0 - DOLFIN_EPS
-u0 = Constant(mesh, 0.0)
+u0 = Constant(0.0)
 bc = DirichletBC(V, u0, DirichletBoundary())
 
-v = TestFunction(V)
-u = TrialFunction(V)
-f = Function(V, "500.0 * exp(-(pow(x[0] - 0.5, 2) + pow(x[1] - 0.5, 2)) / 0.02)")
-a = dot(grad(v), grad(u))*dx
-L = v*f*dx
+A, rhs = assemble_system(a,L,bcs=bc)
+############################################################
 
-A, rhs = assemble_system(a,L,bc,mesh)
+
 
 ############################################################
-# PyAMG
+# Part II: Solve with PyAMG
+from scipy.sparse import csr_matrix
+from pyamg import smoothed_aggregation_solver
 (row,col,data) = A.data()   # get sparse data
 n = A.size(0)
 Asp = csr_matrix( (data,col.view('intc'),row.view('intc')), shape=(n,n))
 b = rhs.data()
 
-residuals = []
-
 ml = smoothed_aggregation_solver(Asp,max_coarse=10)
+residuals = []
 x = ml.solve(b,tol=1e-10,accel='cg',residuals=residuals)
 
 residuals = residuals/residuals[0]
 print ml
+############################################################
 
+
+
+############################################################
+# Part III: plot
+import pylab
 pylab.figure(2)
 pylab.semilogy(residuals)
 pylab.show()
+############################################################
