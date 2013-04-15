13439b74fa568960f94dfeba26d3fe480e41f11d
Luke Olson <luke.olson@gmail.com>
2013-04-15 02:36:44 +0000

    pep8 compliant graph

--- pyamg/graph.py
+++ pyamg/graph.py
@@ -8,7 +8,7 @@ from scipy import sparse
 
 import amg_core
 
-__all__ = ['maximal_independent_set', 'vertex_coloring', 'bellman_ford', \
+__all__ = ['maximal_independent_set', 'vertex_coloring', 'bellman_ford',
            'lloyd_cluster', 'connected_components']
 
 
@@ -20,7 +20,7 @@ def max_value(datatype):
 
 
 def asgraph(G):
-    if not ( sparse.isspmatrix_csr(G) or sparse.isspmatrix_csc(G) ):
+    if not (sparse.isspmatrix_csr(G) or sparse.isspmatrix_csc(G)):
         G = sparse.csr_matrix(G)
 
     if G.shape[0] != G.shape[1]:
@@ -45,15 +45,15 @@ def maximal_independent_set(G, algo='serial', k=None):
 
     Returns
     -------
-    An array S where 
+    An array S where
         S[i] = 1 if vertex i is in the MIS
         S[i] = 0 otherwise
 
     Notes
     -----
     Diagonal entries in the G (self loops) will be ignored.
-    
-    Luby's algorithm is significantly more expensive than the 
+
+    Luby's algorithm is significantly more expensive than the
     greedy serial algorithm.
 
     """
@@ -75,14 +75,13 @@ def maximal_independent_set(G, algo='serial', k=None):
             raise ValueError('unknown algorithm (%s)' % algo)
     else:
         fn = amg_core.maximal_independent_set_k_parallel
-        fn(N, G.indptr, G.indices, k, mis, scipy.rand(N)) 
-
+        fn(N, G.indptr, G.indices, k, mis, scipy.rand(N))
 
     return mis
 
 
 def vertex_coloring(G, method='MIS'):
-    """Compute a vertex coloring of a graph 
+    """Compute a vertex coloring of a graph
 
     Parameters
     ----------
@@ -107,7 +106,7 @@ def vertex_coloring(G, method='MIS'):
 
     G = asgraph(G)
     N = G.shape[0]
-    
+
     coloring = numpy.empty(N, dtype='intc')
 
     if method == 'MIS':
@@ -115,14 +114,15 @@ def vertex_coloring(G, method='MIS'):
         fn(N, G.indptr, G.indices, coloring)
     elif method == 'JP':
         fn = amg_core.vertex_coloring_jones_plassmann
-        fn(N, G.indptr, G.indices, coloring, scipy.rand(N) )
+        fn(N, G.indptr, G.indices, coloring, scipy.rand(N))
     elif method == 'LDF':
         fn = amg_core.vertex_coloring_LDF
-        fn(N, G.indptr, G.indices, coloring, scipy.rand(N) )
+        fn(N, G.indptr, G.indices, coloring, scipy.rand(N))
     else:
         raise ValueError('unknown method (%s)' % method)
 
     return coloring
+
 
 def bellman_ford(G, seeds, maxiter=None):
     """
@@ -146,35 +146,37 @@ def bellman_ford(G, seeds, maxiter=None):
     """
     G = asgraph(G)
     N = G.shape[0]
-    
+
     if maxiter is not None and maxiter < 0:
         raise ValueError('maxiter must be positive')
     if G.dtype == complex:
-        raise ValueError('Bellman-Ford algorithm only defined for real weights')
+        raise ValueError('Bellman-Ford algorithm only defined for real\
+                          weights')
 
     seeds = numpy.asarray(seeds, dtype='intc')
 
-    distances        = numpy.empty(N, dtype=G.dtype)
-    distances[:]     = max_value(G.dtype)
+    distances = numpy.empty(N, dtype=G.dtype)
+    distances[:] = max_value(G.dtype)
     distances[seeds] = 0
 
-    nearest_seed        = numpy.empty(N, dtype='intc')
-    nearest_seed[:]     = -1
+    nearest_seed = numpy.empty(N, dtype='intc')
+    nearest_seed[:] = -1
     nearest_seed[seeds] = seeds
-   
+
     old_distances = numpy.empty_like(distances)
 
     iter = 0
     while maxiter is None or iter < maxiter:
         old_distances[:] = distances
 
-        amg_core.bellman_ford( N, G.indptr, G.indices, G.data,
-                                    distances, nearest_seed)
-        
+        amg_core.bellman_ford(N, G.indptr, G.indices, G.data, distances,
+                              nearest_seed)
+
         if (old_distances == distances).all():
             break
 
-    return (distances,nearest_seed)
+    return (distances, nearest_seed)
+
 
 def lloyd_cluster(G, seeds, maxiter=10):
     """Perform Lloyd clustering on graph with weighted edges
@@ -182,14 +184,14 @@ def lloyd_cluster(G, seeds, maxiter=10):
     Parameters
     ----------
     G : csr_matrix or csc_matrix
-        A sparse NxN matrix where each nonzero entry G[i,j] is the distance 
+        A sparse NxN matrix where each nonzero entry G[i,j] is the distance
         between nodes i and j.
     seeds : {int, array}
-        If seeds is an integer, then its value determines the number of clusters.
-        Otherwise, seeds is an array of unique integers between 0 and N-1 that
-        will be used as the initial seeds for clustering.
+        If seeds is an integer, then its value determines the number of
+        clusters.  Otherwise, seeds is an array of unique integers between 0
+        and N-1 that will be used as the initial seeds for clustering.
     maxiter : int
-        The maximum number of iterations to perform. 
+        The maximum number of iterations to perform.
 
     Notes
     -----
@@ -198,38 +200,38 @@ def lloyd_cluster(G, seeds, maxiter=10):
     """
     G = asgraph(G)
     N = G.shape[0]
-    
+
     if G.dtype.kind == 'c':
         # complex dtype
         G = numpy.abs(G)
-    
+
     #interpret seeds argument
     if numpy.isscalar(seeds):
         seeds = numpy.random.permutation(N)[:seeds]
         seeds = seeds.astype('intc')
-    else: 
+    else:
         seeds = numpy.array(seeds, dtype='intc')
 
     if len(seeds) < 1:
         raise ValueError('at least one seed is required')
-    
+
     if seeds.min() < 0:
         raise ValueError('invalid seed index (%d)' % seeds.min())
     if seeds.max() >= N:
         raise ValueError('invalid seed index (%d)' % seeds.max())
 
-    clusters  = numpy.empty(N, dtype='intc')
+    clusters = numpy.empty(N, dtype='intc')
     distances = numpy.empty(N, dtype=G.dtype)
-    
+
     for i in range(maxiter):
         last_seeds = seeds.copy()
 
-        amg_core.lloyd_cluster(N, G.indptr, G.indices, G.data, \
-                len(seeds), distances, clusters, seeds)
+        amg_core.lloyd_cluster(N, G.indptr, G.indices, G.data,
+                               len(seeds), distances, clusters, seeds)
 
         if (seeds == last_seeds).all():
             break
-    
+
     return (distances, clusters, seeds)
 
 
@@ -266,13 +268,14 @@ def breadth_first_search(G, seed):
     BFS = amg_core.breadth_first_search
     BFS(G.indptr, G.indices, int(seed), order, level)
 
-    return order,level
+    return order, level
+
 
 def connected_components(G):
     """Compute the connected components of a graph
 
-    The connected components of a graph G, which is represented by a 
-    symmetric sparse matrix, are labeled with the integers 0,1,..(K-1) where 
+    The connected components of a graph G, which is represented by a
+    symmetric sparse matrix, are labeled with the integers 0,1,..(K-1) where
     K is the number of components.
 
     Parameters
@@ -287,7 +290,7 @@ def connected_components(G):
 
     Notes
     -----
-    If the nonzero structure of G is not symmetric, then the 
+    If the nonzero structure of G is not symmetric, then the
     result is undefined.
 
     Examples
@@ -300,18 +303,18 @@ def connected_components(G):
     [0 1 2]
     >>> print connected_components( [[0,1,0,0],[1,0,0,0],[0,0,0,1],[0,0,1,0]] )
     [0 0 1 1]
-
-    """    
+    """
     G = asgraph(G)
     N = G.shape[0]
 
     #Check symmetry?
     components = numpy.empty(N, G.indptr.dtype)
-    
+
     fn = amg_core.connected_components
     fn(N, G.indptr, G.indices, components)
 
     return components
+
 
 def symmetric_rcm(A):
     """
@@ -325,7 +328,7 @@ def symmetric_rcm(A):
     >>> import pylab
     >>> from pyamg import gallery
     >>> from pyamg.graph import symmetric_rcm
-    >>> n = 200 
+    >>> n = 200
     >>> density = 1.0/n
     >>> A = gallery.sprand(n, n, density, format='csr')
     >>> S = A + A.T
@@ -346,10 +349,11 @@ def symmetric_rcm(A):
 
     Perm = sparse.identity(n)
     p = level.argsort()
-    Perm = Perm[p,:]
+    Perm = Perm[p, :]
 
-    return Perm*A*Perm.T
-    
+    return Perm * A * Perm.T
+
+
 def pseudo_peripheral_node(A):
     """
     Algorithm in Saad
@@ -366,7 +370,7 @@ def pseudo_peripheral_node(A):
 
     while 1:
         # do a level-set traversal from x
-        order, level = breadth_first_search(A,x)
+        order, level = breadth_first_search(A, x)
 
         # select a node y in the last level with min degree
         maxlevel = level.max()
@@ -377,7 +381,7 @@ def pseudo_peripheral_node(A):
         y = lastnodes[y]
 
         # if d(x,y)>delta, set, and go to bfs above
-        if level[y]>delta:
+        if level[y] > delta:
             x = y
             delta = level[y]
         else:
