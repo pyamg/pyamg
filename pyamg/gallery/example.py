800e53ffdb3aaf3fb0751e532a25663d66e2bb45
Luke Olson <luke.olson@gmail.com>
2013-12-05 21:44:55 +0100

    Changes some spacing in example to make pep8 compliant

--- pyamg/gallery/example.py
+++ pyamg/gallery/example.py
@@ -12,11 +12,12 @@ from scipy.io import loadmat
 __all__ = ['load_example']
 
 base_dir = os.path.split(__file__)[0]
-example_dir = os.path.join( base_dir, 'example_data')
-example_files = glob( os.path.join(example_dir, '*.mat') )
-example_names = [ os.path.split(name)[1][:-4] for name in example_files ]
+example_dir = os.path.join(base_dir, 'example_data')
+example_files = glob(os.path.join(example_dir, '*.mat'))
+example_names = [os.path.split(name)[1][:-4] for name in example_files]
 
 example_names.sort()
+
 
 def load_example(name):
     """Load an example problem by name
@@ -25,7 +26,7 @@ def load_example(name):
     ----------
     name : string (e.g. 'airfoil')
         Name of the example to load
-        
+
     Notes
     -----
     Each example is stored in a dictionary with the following keys:
@@ -41,12 +42,13 @@ def load_example(name):
     >>> from pyamg.gallery import load_example
     >>> ex = load_example('knot')
 
-    """ 
-    
+    """
+
     if name not in example_names:
         raise ValueError('no example with name (%s)' % name)
     else:
-        return loadmat(os.path.join(example_dir, name + '.mat'), struct_as_record=True)
-   
+        return loadmat(os.path.join(example_dir, name + '.mat'),
+                       struct_as_record=True)
+
 # insert the example names into the docstring
-load_example.__doc__ %= ('\n' + ' ' * 8).join( [''] + example_names)
+load_example.__doc__ %= ('\n' + ' ' * 8).join([''] + example_names)
