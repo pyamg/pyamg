4c6ddb7b093f339da1450899532429311eb219d7
Luke Olson <luke.olson@gmail.com>
2013-12-05 21:41:35 +0100

    Changes mesh to make compliant.

--- pyamg/gallery/mesh.py
+++ pyamg/gallery/mesh.py
@@ -2,13 +2,14 @@
 
 __docformat__ = "restructuredtext en"
 
-import numpy
+import numpy as np
 
 __all__ = ['regular_triangle_mesh']
 
+
 def regular_triangle_mesh(nx, ny):
     """Construct a regular triangular mesh in the unit square
-    
+
     Parameters
     ----------
     nx : int
@@ -29,24 +30,27 @@ def regular_triangle_mesh(nx, ny):
     >>> E2V,Vert = regular_triangle_mesh(3, 2)
 
     """
-    nx,ny = int(nx),int(ny)
+    nx, ny = int(nx), int(ny)
 
     if nx < 2 or ny < 2:
-        raise ValueError('minimum mesh dimension is 2: %s' % ((nx,ny),) )
+        raise ValueError('minimum mesh dimension is 2: %s' % ((nx, ny),))
 
-    Vert1 = numpy.tile(numpy.arange(0, nx-1), ny - 1) + numpy.repeat(numpy.arange(0, nx * (ny - 1), nx), nx - 1)
-    Vert3 = numpy.tile(numpy.arange(0, nx-1), ny - 1) + numpy.repeat(numpy.arange(0, nx * (ny - 1), nx), nx - 1) + nx
+    Vert1 = np.tile(np.arange(0, nx-1), ny - 1) +\
+        np.repeat(np.arange(0, nx * (ny - 1), nx), nx - 1)
+    Vert3 = np.tile(np.arange(0, nx-1), ny - 1) +\
+        np.repeat(np.arange(0, nx * (ny - 1), nx), nx - 1) + nx
     Vert2 = Vert3 + 1
     Vert4 = Vert1 + 1
 
-    Verttmp = numpy.meshgrid(numpy.arange(0, nx, dtype='float'), numpy.arange(0, ny, dtype='float'))
-    Verttmp = (Verttmp[0].ravel(),Verttmp[1].ravel())
-    Vert = numpy.vstack(Verttmp).transpose()
-    Vert[:,0] = (1.0 / (nx - 1)) * Vert[:,0]
-    Vert[:,1] = (1.0 / (ny - 1)) * Vert[:,1]
+    Verttmp = np.meshgrid(np.arange(0, nx, dtype='float'),
+                          np.arange(0, ny, dtype='float'))
+    Verttmp = (Verttmp[0].ravel(), Verttmp[1].ravel())
+    Vert = np.vstack(Verttmp).transpose()
+    Vert[:, 0] = (1.0 / (nx - 1)) * Vert[:, 0]
+    Vert[:, 1] = (1.0 / (ny - 1)) * Vert[:, 1]
 
-    E2V1 = numpy.vstack((Vert1,Vert2,Vert3)).transpose()
-    E2V2 = numpy.vstack((Vert1,Vert4,Vert2)).transpose()
-    E2V = numpy.vstack((E2V1,E2V2))
+    E2V1 = np.vstack((Vert1, Vert2, Vert3)).transpose()
+    E2V2 = np.vstack((Vert1, Vert4, Vert2)).transpose()
+    E2V = np.vstack((E2V1, E2V2))
 
-    return Vert,E2V
+    return Vert, E2V
