a601a5a0f6325e42c7fdf073127f2aec8ca9b2bc
Luke Olson <luke.olson@gmail.com>
2015-11-21 09:48:44 -0600

    fix #157 householder as default

--- pyamg/krylov/_gmres.py
+++ pyamg/krylov/_gmres.py
@@ -8,7 +8,7 @@ __all__ = ['gmres']
 
 
 def gmres(A, b, x0=None, tol=1e-5, restrt=None, maxiter=None, xtype=None,
-          M=None, callback=None, residuals=None, orthog='mgs', **kwargs):
+          M=None, callback=None, residuals=None, orthog='householder', **kwargs):
     '''
     Generalized Minimum Residual Method (GMRES)
         GMRES iteratively refines the initial solution guess to the
