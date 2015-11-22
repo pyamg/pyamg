9af2b42aae146aab11a32f43e837e7523f1e772f
Luke Olson <luke.olson@gmail.com>
2015-11-21 21:01:42 -0600

    add 3.5 to pypi

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 Topic :: Education
 Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Mathematics
