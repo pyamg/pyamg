2a9124bcef69278eac3089bbf1e44782b7b015b0
Southworth <ben.s.southworth@gmail.com>
2016-06-07 13:09:00 -0700

    Added exception handling if iterations or sweep arguments are not passed in.

--- pyamg/relaxation/smoothing.py
+++ pyamg/relaxation/smoothing.py
@@ -167,12 +167,34 @@ def change_smoothers(ml, presmoother, postsmoother):
         ml.levels[i].postsmoother = setup_postsmoother(ml.levels[i], **kwargs2)
 
         # Check if symmetric smoothing scheme
-        if (fn1 != fn2) or (kwargs1['iterations'] != kwargs2['iterations']):
+        try:
+            it1 = kwargs1['iterations']
+        except:
+            it1 = 1
+        try:
+            it2 = kwargs2['iterations']
+        except:
+            it2 = 1
+        if (fn1 != fn2) or (it1 != it2):
             ml.symmetric_smoothing = False
         elif (fn1 != 'jacobi') and (fn1 != 'richardson') and \
              (fn1 != 'block_jacobi') and (fn1 != 'jacobi_ne'):
-             if (kwargs1['sweep'] != 'symmetric') or \
-                (kwargs2['sweep'] != 'symmetric'):
+            try:
+                sweep1 = kwargs1['sweep']
+            except:
+                if (fn1 == 'strength_based_schwarz') or (fn1 == 'schwarz'):
+                    sweep1 = 'symmetric'
+                else:
+                    sweep1 = 'forward'
+            try:
+                sweep2 = kwargs2['sweep']
+            except:
+                if (fn2 == 'strength_based_schwarz') or (fn2 == 'schwarz'):
+                    sweep2 = 'symmetric'
+                else:
+                    sweep2 = 'forward'
+
+             if (sweep1 != 'symmetric') or (sweep2 != 'symmetric'):
                 ml.symmetric_smoothing = False
 
     if len(presmoother) < len(postsmoother):
@@ -191,12 +213,34 @@ def change_smoothers(ml, presmoother, postsmoother):
             ml.levels[i].postsmoother = setup_postsmoother(ml.levels[i], **kwargs2)
 
             # Check if symmetric smoothing scheme
-            if (fn1 != fn2) or (kwargs1['iterations'] != kwargs2['iterations']):
+            try:
+                it1 = kwargs1['iterations']
+            except:
+                it1 = 1
+            try:
+                it2 = kwargs2['iterations']
+            except:
+                it2 = 1
+            if (fn1 != fn2) or (it1 != it2):
                 ml.symmetric_smoothing = False
             elif (fn1 != 'jacobi') and (fn1 != 'richardson') and \
                  (fn1 != 'block_jacobi') and (fn1 != 'jacobi_ne'):
-                 if (kwargs1['sweep'] != 'symmetric') or \
-                    (kwargs2['sweep'] != 'symmetric'):
+                try:
+                    sweep1 = kwargs1['sweep']
+                except:
+                    if (fn1 == 'strength_based_schwarz') or (fn1 == 'schwarz'):
+                        sweep1 = 'symmetric'
+                    else:
+                        sweep1 = 'forward'
+                try:
+                    sweep2 = kwargs2['sweep']
+                except:
+                    if (fn2 == 'strength_based_schwarz') or (fn2 == 'schwarz'):
+                        sweep2 = 'symmetric'
+                    else:
+                        sweep2 = 'forward'
+
+                 if (sweep1 != 'symmetric') or (sweep2 != 'symmetric'):
                     ml.symmetric_smoothing = False
 
     elif len(presmoother) > len(postsmoother):
@@ -215,14 +259,36 @@ def change_smoothers(ml, presmoother, postsmoother):
             ml.levels[i].postsmoother = setup_postsmoother(ml.levels[i], **kwargs2)
 
             # Check if symmetric smoothing scheme
-            if (fn1 != fn2) or (kwargs1['iterations'] != kwargs2['iterations']):
+            try:
+                it1 = kwargs1['iterations']
+            except:
+                it1 = 1
+            try:
+                it2 = kwargs2['iterations']
+            except:
+                it2 = 1
+            if (fn1 != fn2) or (it1 != it2):
                 ml.symmetric_smoothing = False
             elif (fn1 != 'jacobi') and (fn1 != 'richardson') and \
                  (fn1 != 'block_jacobi') and (fn1 != 'jacobi_ne'):
-                 if (kwargs1['sweep'] != 'symmetric') or \
-                    (kwargs2['sweep'] != 'symmetric'):
-                    ml.symmetric_smoothing = False       
+                try:
+                        sweep1 = kwargs1['sweep']
+                    except:
+                        if (fn1 == 'strength_based_schwarz') or (fn1 == 'schwarz'):
+                            sweep1 = 'symmetric'
+                        else:
+                            sweep1 = 'forward'
+                    try:
+                        sweep2 = kwargs2['sweep']
+                    except:
+                        if (fn2 == 'strength_based_schwarz') or (fn2 == 'schwarz'):
+                            sweep2 = 'symmetric'
+                        else:
+                            sweep2 = 'forward'
 
+                     if (sweep1 != 'symmetric') or (sweep2 != 'symmetric'):
+                        ml.symmetric_smoothing = False     
+                        
     else:  
         mid_len = min_len
 
