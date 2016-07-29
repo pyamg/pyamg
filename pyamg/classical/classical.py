70e7e4bef6b50a0b103142ab00898c2121c69264
Ben Southworth <ben.s.southworth@gmail.com>
2016-07-29 11:04:17 -0700

    Update classical AMG (#177)
    Update classical AMG to accommodate CR as a splitting scheme

--- pyamg/classical/classical.py
+++ pyamg/classical/classical.py
@@ -15,6 +15,7 @@ from pyamg.strength import classical_strength_of_connection, \
 
 from .interpolate import direct_interpolation
 from . import split
+from .cr import CR
 
 __all__ = ['ruge_stuben_solver']
 
@@ -31,15 +32,15 @@ def ruge_stuben_solver(A,
     ----------
     A : csr_matrix
         Square matrix in CSR format
-    strength : ['symmetric', 'classical', 'evolution', 'algebraic_distance',
-                'affinity', None]
+    strength : ['symmetric', 'classical', 'evolution', 'distance',
+                'algebraic_distance','affinity', 'energy_based', None]
         Method used to determine the strength of connection between unknowns
         of the linear system.  Method-specific parameters may be passed in
         using a tuple, e.g. strength=('symmetric',{'theta' : 0.25 }). If
         strength=None, all nonzero entries of the matrix are considered strong.
     CF : {string} : default 'RS'
         Method used for coarse grid selection (C/F splitting)
-        Supported methods are RS, PMIS, PMISc, CLJP, and CLJPc
+        Supported methods are RS, PMIS, PMISc, CLJP, CLJPc, and CR.
     presmoother : {string or dict}
         Method used for presmoothing at each level.  Method-specific parameters
         may be passed in using a tuple, e.g.
@@ -119,6 +120,7 @@ def ruge_stuben_solver(A,
 # internal function
 def extend_hierarchy(levels, strength, CF, keep):
     """ helper function for local methods """
+
     def unpack_arg(v):
         if isinstance(v, tuple):
             return v[0], v[1]
@@ -153,15 +155,17 @@ def extend_hierarchy(levels, strength, CF, keep):
     # Generate the C/F splitting
     fn, kwargs = unpack_arg(CF)
     if fn == 'RS':
-        splitting = split.RS(C)
+        splitting = split.RS(C, **kwargs)
     elif fn == 'PMIS':
-        splitting = split.PMIS(C)
+        splitting = split.PMIS(C, **kwargs)
     elif fn == 'PMISc':
-        splitting = split.PMISc(C)
+        splitting = split.PMISc(C, **kwargs)
     elif fn == 'CLJP':
-        splitting = split.CLJP(C)
+        splitting = split.CLJP(C, **kwargs)
     elif fn == 'CLJPc':
-        splitting = split.CLJPc(C)
+        splitting = split.CLJPc(C, **kwargs)
+    elif fn == 'CR':
+        splitting = CR(C, **kwargs)
     else:
         raise ValueError('unknown C/F splitting method (%s)' % CF)
 
