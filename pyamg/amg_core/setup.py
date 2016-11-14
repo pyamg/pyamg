3cb3f01cc97d202dd4305a86230ed2dcf54b2b3a
lukeolson <luke.olson@gmail.com>
2016-11-14 12:35:45 -0600

    trying things

--- pyamg/amg_core/setup.py
+++ pyamg/amg_core/setup.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     from setuptools import setup, Extension
     import numpy as np
 
-    setup(ext_modules=[Extension('amg_core.amg_core',
+    setup(ext_modules=[Extension('_amg_core',
                                  sources=['amg_core_wrap.cxx'],
                                  define_macros=[('__STDC_FORMAT_MACROS', 1)],
                                  include_dirs=[np.get_include()])],
