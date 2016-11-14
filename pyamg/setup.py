078d4b05fcc250435b352b03e9c2017f567f7991
lukeolson <luke.olson@gmail.com>
2016-11-14 12:28:06 -0600

    trying things

--- pyamg/setup.py
+++ pyamg/setup.py
@@ -22,5 +22,10 @@ def configuration(parent_package='', top_path=None):
     return config
 
 if __name__ == '__main__':
-    from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+    # from numpy.distutils.core import setup
+    # setup(**configuration(top_path='').todict())
+
+    from setuptools import setup
+    import setuptools
+    packages = setuptools.find_packages(exclude=['doc'])
+    setup(packages=packages)
