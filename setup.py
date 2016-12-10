da112e1581be3f5d241a39ab0a76181c52663718
lukeolson <luke.olson@gmail.com>
2016-12-10 09:15:57 -0600

    abosrb version tools into setup

--- setup.py
+++ setup.py
@@ -19,7 +19,9 @@ supporting C++ code for performance critical operations.
 """
 
 import io
-import version_tools
+import os
+import subprocess
+
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext as _build_ext
 from setuptools.command.test import test as TestCommand
@@ -27,13 +29,104 @@ from setuptools.command.test import test as TestCommand
 version = '3.2.0'
 isreleased = False
 
+install_requires = (
+    'numpy>=1.7.0',
+    'scipy>=0.12.0',
+    'pytest>=2'
+)
+
+
 # set the version information
-fullversion, git_revision, git_branch =\
-    version_tools.set_version_info(version, isreleased)
-version_tools.write_version_py(version, fullversion,
-                               git_revision, git_branch,
-                               isreleased,
-                               filename='pyamg/version.py')
+# https://github.com/numpy/numpy/commits/master/setup.py
+# Return the git revision as a string
+
+
+def git_version():
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+        out = _minimal_ext_cmd(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+        GIT_BRANCH = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = 'Unknown'
+        GIT_BRANCH = ''
+
+    return GIT_REVISION, GIT_BRANCH
+
+
+def set_version_info(VERSION, ISRELEASED):
+    # Adding the git rev number needs to be done inside write_version_py(),
+    # otherwise the import of numpy.version messes up the build under Python 3.
+
+    if os.path.exists('.git'):
+        GIT_REVISION, GIT_BRANCH = git_version()
+    elif os.path.exists('pyamg/version.py'):
+        # must be a source distribution, use existing version file
+        try:
+            from pyamg.version import git_revision as GIT_REVISION
+            from pyamg.version import git_branch as GIT_BRANCH
+        except ImportError:
+            raise ImportError('Unable to import git_revision. Try removing '
+                              'pyamg/version.py and the build directory '
+                              'before building.')
+    else:
+        GIT_REVISION = 'Unknown'
+        GIT_BRANCH = ''
+
+    FULLVERSION = VERSION
+    if not ISRELEASED:
+        FULLVERSION += '.dev0' + '+' + GIT_REVISION[:7]
+
+    return FULLVERSION, GIT_REVISION, GIT_BRANCH
+
+
+def write_version_py(VERSION,
+                     FULLVERSION,
+                     GIT_REVISION,
+                     GIT_BRANCH,
+                     ISRELEASED,
+                     filename='pyamg/version.py'):
+    cnt = """
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+git_branch = '%(git_branch)s'
+release = %(isrelease)s
+if not release:
+    version = full_version
+"""
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'git_branch': GIT_BRANCH,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
+
+fullversion, git_revision, git_branch = set_version_info(version, isreleased)
+version_tools.write_version_py(version, fullversion, git_revision, git_branch,
+                               isreleased, filename='pyamg/version.py')
 
 
 # identify extension modules
@@ -82,7 +175,7 @@ setup(
     packages=find_packages(exclude=['doc']),
     package_data={'pyamg': ['gallery/example_data/*.mat']},
     include_package_data=False,
-    install_requires=io.open('requirements.txt').read().split('\n'),
+    install_requires=install_requires,
     zip_safe=False,
     #
     ext_modules=ext_modules,
@@ -92,27 +185,27 @@ setup(
     tests_require=['pytest'],
     #
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Environment :: X11 Applications",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: C++",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Topic :: Education",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: X11 Applications',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
