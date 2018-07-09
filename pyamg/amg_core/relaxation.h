ca691d25958016a5aa598e190af026ee36a09586
Luke Olson <luke.olson@gmail.com>
2018-07-09 13:42:19 -0500

    add multi-vector support to jacobi_m

--- pyamg/amg_core/relaxation.h
+++ pyamg/amg_core/relaxation.h
@@ -2,6 +2,8 @@
 #define RELAXATION_H
 
 #include "linalg.h"
+#include <algorithm>
+#include <vector>
 
 /*
  * Perform one iteration of Gauss-Seidel relaxation on the linear
@@ -346,9 +348,9 @@ template<class I, class T, class F>
 void jacobi_m(const I Ap[], const int Ap_size,
             const I Aj[], const int Aj_size,
             const T Ax[], const int Ax_size,
-                  T  x[], const int  x_size,
-            const T  b[], const int  b_size,
-                  T temp[], const int temp_size,
+                  T  x[], const int  x_size0, const int x_size1,
+            const T  b[], const int  b_size0, const int b_size1,
+                  T temp[], const int temp_size0, const int temp_size1,
             const I row_start,
             const I row_stop,
             const I row_step,
@@ -356,15 +358,19 @@ void jacobi_m(const I Ap[], const int Ap_size,
 {
     T one = 1.0;
     T omega2 = omega[0];
+    I s = x_size1;
+    std::vector<T> rsum(s);
 
     for(I i = row_start; i != row_stop; i += row_step) {
-        temp[i] = x[i];
+        for(I si = 0; si < s; si++) {
+            temp[i*s + si] = x[i*s + si];
+        }
     }
 
     for(I i = row_start; i != row_stop; i += row_step) {
         I start = Ap[i];
         I end   = Ap[i+1];
-        T rsum = 0;
+        std::fill(rsum.begin(), rsum.end(), 0);
         T diag = 0;
 
         for(I jj = start; jj < end; jj++){
@@ -372,11 +378,15 @@ void jacobi_m(const I Ap[], const int Ap_size,
             if (i == j)
                 diag  = Ax[jj];
             else
-                rsum += Ax[jj]*temp[j];
+                for(I si=0; si < s; si++){
+                    rsum[si] += Ax[jj]*temp[j*s + si];
+                }
         }
 
         if (diag != (F) 0.0){
-            x[i] = (one - omega2) * temp[i] + omega2 * ((b[i] - rsum)/diag);
+            for(I si=0; si < s; si++){
+                x[i*s + si] = (one - omega2) * temp[i*s + si] + omega2 * ((b[i*s + si] - rsum[si])/diag);
+            }
         }
     }
 }
