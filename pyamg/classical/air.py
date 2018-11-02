0aa5d079c78d3c09563f04e63a85046f59d16162
Ben Southworth <ben.southworth@colorado.edu>
2018-11-02 15:21:08 -0700

    added python AIR files

---
+++ pyamg/classical/air.py
@@ -0,0 +1,253 @@
+"""Approximate ideal restriction AMG"""
+from __future__ import absolute_import
+
+__docformat__ = "restructuredtext en"
+
+from warnings import warn
+from scipy.sparse import csr_matrix, bsr_matrix, isspmatrix_csr, isspmatrix_bsr, \
+    SparseEfficiencyWarning, block_diag
+import numpy as np
+from copy import deepcopy
+
+from pyamg.multilevel import multilevel_solver
+from pyamg.relaxation.smoothing import change_smoothers
+from pyamg.strength import classical_strength_of_connection, \
+    symmetric_strength_of_connection, evolution_strength_of_connection, \
+    distance_strength_of_connection, energy_based_strength_of_connection, \
+    algebraic_distance, affinity_distance
+from pyamg.util.utils import mat_mat_complexity, unpack_arg, extract_diagonal_blocks, \
+    filter_matrix_rows
+from pyamg.classical.interpolate import direct_interpolation,
+     one_point_interpolation, injection_interpolation, local_AIR, \
+     neumann_AIR
+from pyamg.classical.split import *
+from pyamg.classical.cr import CR
+
+__all__ = ['AIR_solver']
+
+def AIR_solver(A,
+               strength=('classical', {'theta': 0.3 ,'norm': 'min'}),
+               CF='RS',
+               interp='one_point',
+               restrict='neumann',
+               presmoother=None,
+               postsmoother=('FC_jacobi', {'omega': 1.0, 'iterations': 1,
+                              'withrho': False,  'F_iterations': 2,
+                              'C_iterations': 0} ),
+               filter_operator=None,
+               coarse_grid_P=None, 
+               coarse_grid_R=None, 
+               max_levels=20, max_coarse=20,
+               keep=False, **kwargs):
+    """Create a multilevel solver using Classical AMG (Ruge-Stuben AMG)
+
+    Parameters
+    ----------
+    A : csr_matrix
+        Square nonsymmetric matrix in CSR format
+    strength : ['symmetric', 'classical', 'evolution', 'distance',
+                'algebraic_distance','affinity', 'energy_based', None]
+        Method used to determine the strength of connection between unknowns
+        of the linear system.  Method-specific parameters may be passed in
+        using a tuple, e.g. strength=('symmetric',{'theta' : 0.25 }). If
+        strength=None, all nonzero entries of the matrix are considered strong.
+    CF : {string} : default 'RS'
+        Method used for coarse grid selection (C/F splitting)
+        Supported methods are RS, PMIS, PMISc, CLJP, CLJPc, and CR.
+    interp : {string} : default 'one-point'
+        Options include 'direct', 'standard', 'inject' and 'one-point'.
+    restrict : {string} : default 'neumann'
+        Options include 'air' for approximate ideal
+        restriction.
+    presmoother : {string or dict} : default None
+        Method used for presmoothing at each level.  Method-specific parameters
+        may be passed in using a tuple, e.g.
+        presmoother=('gauss_seidel',{'sweep':'symmetric}), the default.
+    postsmoother : {string or dict} : default F-Jacobi
+        Postsmoothing method with the same usage as presmoother
+    filter_operator : (bool, tol) : default None
+        Remove small entries in operators on each level if True. Entries are
+        considered "small" if |a_ij| < tol |a_ii|.
+    coarse_grid_P : {string} : default None
+        Option to specify a different construction of P used in computing RAP
+        vs. for interpolation in an actual solve.
+    max_levels: {integer} : default 20
+        Maximum number of levels to be used in the multilevel solver.
+    max_coarse: {integer} : default 20
+        Maximum number of variables permitted on the coarse grid.
+    keep: {bool} : default False
+        Flag to indicate keeping extra operators in the hierarchy for
+        diagnostics.  For example, if True, then strength of connection (C) and
+        tentative prolongation (T) are kept.
+
+    Returns
+    -------
+    ml : multilevel_solver
+        Multigrid hierarchy of matrices and prolongation operators
+
+    Other Parameters
+    ----------------
+    coarse_solver : ['splu', 'lu', 'cholesky, 'pinv', 'gauss_seidel', ... ]
+        Solver used at the coarsest level of the MG hierarchy.
+            Optionally, may be a tuple (fn, args), where fn is a string such as
+        ['splu', 'lu', ...] or a callable function, and args is a dictionary of
+        arguments to be passed to fn.
+    setup_complexity : bool
+        For a detailed, more accurate setup complexity, pass in 
+        'setup_complexity' = True. This will slow down performance, but
+        increase accuracy of complexity count. 
+
+    Notes
+    -----
+
+
+
+
+    References
+    ----------
+    .. [1] 
+
+    See Also
+    --------
+    aggregation.smoothed_aggregation_solver, multilevel_solver,
+    aggregation.rootnode_solver
+
+    """
+
+    if ('setup_complexity' in kwargs):
+        if kwargs['setup_complexity'] == True:
+            mat_mat_complexity.__detailed__ = True
+        del kwargs['setup_complexity']
+
+    # preprocess A
+    A = A.asfptype()
+    if A.shape[0] != A.shape[1]:
+        raise ValueError('expected square matrix')
+
+    levels = [multilevel_solver.level()]
+    levels[-1].A = A
+
+    while len(levels) < max_levels and levels[-1].A.shape[0] > max_coarse:
+        bottom = extend_hierarchy(levels, strength, CF, interp, restrict, filter_operator,
+                                  coarse_grid_P, coarse_grid_R, keep)
+        if bottom:
+            break
+
+    ml = multilevel_solver(levels, **kwargs)
+    change_smoothers(ml, presmoother, postsmoother)
+    return ml
+
+
+# internal function
+def extend_hierarchy(levels, strength, CF, interp, restrict, filter_operator,
+                     coarse_grid_P, coarse_grid_R, keep):
+    """ helper function for local methods """
+
+    # Filter operator. Need to keep original matrix on finest level for
+    # computing residuals
+    if (filter_operator is not None) and (filter_operator[1] != 0): 
+        if len(levels) == 1:
+            A = deepcopy(levels[-1].A)
+        else:
+            A = levels[-1].A
+        filter_matrix_rows(A, filter_operator[1], diagonal=True, lump=filter_operator[0])
+    else:
+        A = levels[-1].A
+
+    # Check if matrix was filtered to be diagonal --> coarsest grid
+    if A.nnz == A.shape[0]:
+        return 1
+
+    # Compute the strength-of-connection matrix C, where larger
+    # C[i,j] denote stronger couplings between i and j.
+    fn, kwargs = unpack_arg(strength)
+    if fn == 'symmetric':
+        C = symmetric_strength_of_connection(A, **kwargs)
+    elif fn == 'classical':
+        C = classical_strength_of_connection(A, **kwargs)
+    elif fn == 'distance':
+        C = distance_strength_of_connection(A, **kwargs)
+    elif (fn == 'ode') or (fn == 'evolution'):
+        C = evolution_strength_of_connection(A, **kwargs)
+    elif fn == 'energy_based':
+        C = energy_based_strength_of_connection(A, **kwargs)
+    elif fn == 'algebraic_distance':
+        C = algebraic_distance(A, **kwargs)
+    elif fn == 'affinity':
+        C = affinity_distance(A, **kwargs)
+    elif fn is None:
+        C = A
+    else:
+        raise ValueError('unrecognized strength of connection method: %s' %
+                         str(fn))
+
+    # Generate the C/F splitting
+    fn, kwargs = unpack_arg(CF)
+    if fn == 'RS':
+        splitting = RS(C, **kwargs)
+    elif fn == 'PMIS':
+        splitting = PMIS(C, **kwargs)
+    elif fn == 'PMISc':
+        splitting = PMISc(C, **kwargs)
+    elif fn == 'CLJP':
+        splitting = CLJP(C, **kwargs)
+    elif fn == 'CLJPc':
+        splitting = CLJPc(C, **kwargs)
+    elif fn == 'CR':
+        splitting = CR(C, **kwargs)
+    elif fn == 'weighted_matching':
+        splitting, soc = weighted_matching(C, **kwargs)
+        if soc is not None:
+            C = soc
+    else:
+        raise ValueError('unknown C/F splitting method (%s)' % CF)
+
+    # Generate the interpolation matrix that maps from the coarse-grid to the
+    # fine-grid
+    r_flag = False
+    fn, kwargs = unpack_arg(interp)
+    if fn == 'standard':
+        P = standard_interpolation(A, C, splitting, **kwargs)
+    elif fn == 'distance_two':
+        P = distance_two_interpolation(A, C, splitting, **kwargs)
+    elif fn == 'direct':
+        P = direct_interpolation(A, C, splitting, **kwargs)
+    elif fn == 'one_point':
+        P = one_point_interpolation(A, C, splitting, **kwargs)
+    elif fn == 'inject':
+        P = injection_interpolation(A, splitting, **kwargs)
+    else:
+        raise ValueError('unknown interpolation method (%s)' % interp)
+
+    # Build restriction operator
+    fn, kwargs = unpack_arg(restrict)
+    if fn is None:
+        R = P.T
+    elif fn == 'air':
+        R = local_AIR(A, splitting, **kwargs)
+    elif fn == 'neumann':
+        R = neumann_AIR(A, splitting, **kwargs)
+    else:
+        raise ValueError('unknown restriction method (%s)' % restrict)
+
+    # Store relevant information for this level
+    if keep:
+        levels[-1].C = C              # strength of connection matrix
+
+    levels[-1].P = P                  # prolongation operator
+    levels[-1].R = R                  # restriction operator
+    levels[-1].splitting = splitting  # C/F splitting
+
+    # RL: RAP = R*(A*P)
+    A = R * A * P
+
+    # Make sure coarse-grid operator is in correct sparse format
+    if (isspmatrix_csr(P) and (not isspmatrix_csr(A))):
+        A = A.tocsr()
+    elif (isspmatrix_bsr(P) and (not isspmatrix_bsr(A))):
+        A = A.tobsr()
+
+    levels.append(multilevel_solver.level())
+    levels[-1].A = A
+    return 0
+
--- pyamg/classical/interpolate.py
+++ pyamg/classical/interpolate.py
@@ -4,8 +4,12 @@
 import numpy as np
 from scipy.sparse import csr_matrix, isspmatrix_csr
 from pyamg import amg_core
+from pyamg.util.utils import filter_matrix_rows, UnAmal
+from pyamg.strength import classical_strength_of_connection
 
-__all__ = ['direct_interpolation']
+
+__all__ = ['direct_interpolation','injection_interpolation',
+           'one_point_interpolation','neumann_AIR', 'local_AIR']
 
 
 def direct_interpolation(A, C, splitting):
@@ -71,3 +75,293 @@ def direct_interpolation(A, C, splitting):
                                            Pp, Pj, Px)
 
     return csr_matrix((Px, Pj, Pp))
+
+
+def injection_interpolation(A, splitting):
+    """ Create interpolation operator by injection, that is C-points are
+    interpolated by value and F-points are not interpolated.
+
+    Parameters
+    ----------
+    A : {csr_matrix}
+        NxN matrix in CSR format or BSR format
+    splitting : array
+        C/F splitting stored in an array of length N
+
+    Returns
+    -------
+    NxNc interpolation operator, P
+    """
+    if isspmatrix_bsr(A):
+        blocksize = A.blocksize[0]
+        n = A.shape[0] / blocksize
+    elif isspmatrix_csr(A):
+        n = A.shape[0]
+        blocksize = 1
+    else:
+        try:
+            A = A.tocsr()
+            warn("Implicit conversion of A to csr", SparseEfficiencyWarning)
+            n = A.shape[0]
+            blocksize = 1
+        except:
+            raise TypeError("Invalid matrix type, must be CSR or BSR.")
+
+    P_rowptr = np.append(np.array([0],dtype='int32'), np.cumsum(splitting,dtype='int32') )
+    nc = P_rowptr[-1]
+    P_colinds = np.arange(start=0, stop=nc, step=1, dtype='int32')
+
+    if blocksize == 1:
+        return csr_matrix((np.ones((nc,), dtype=A.dtype), P_colinds, P_rowptr), shape=[n,nc])
+    else:
+        P_data = np.array(nc*[np.identity(blocksize, dtype=A.dtype)], dtype=A.dtype)
+        return bsr_matrix((P_data, P_colinds, P_rowptr), blocksize=[blocksize,blocksize],
+                          shape=[n*blocksize,nc*blocksize])
+
+
+def one_point_interpolation(A, C, splitting, by_val=False):
+    """ Create one-point interpolation operator, that is C-points are
+    interpolated by value and F-points are interpolated by value from
+    their strongest-connected C-point neighbor.
+
+    Parameters
+    ----------
+    A : {csr_matrix}
+        NxN matrix in CSR format
+    C : {csr_matrix}
+        Strength-of-Connection matrix (does not need zero diagonal)
+    by_val : bool
+        For CSR matrices only right now, use values of -Afc in interp as an
+        approximation to P_ideal. If false, F-points are interpolated by value
+        with weight 1.
+    splitting : array
+        C/F splitting stored in an array of length N
+
+    Returns
+    -------
+    NxNc interpolation operator, P
+    """
+    if isspmatrix_bsr(A):
+        blocksize = A.blocksize[0]
+        n = A.shape[0] / blocksize
+    elif isspmatrix_csr(A):
+        n = A.shape[0]
+        blocksize = 1
+    else:
+        try:
+            A = A.tocsr()
+            warn("Implicit conversion of A to csr", SparseEfficiencyWarning)
+            n = A.shape[0]
+            blocksize = 1
+        except:
+            raise TypeError("Invalid matrix type, must be CSR or BSR.")
+
+    nc = np.sum(splitting)
+    P_rowptr = np.empty((n+1,), dtype='int32') # P: n x nc, at most 'n' nnz
+    P_colinds = np.empty((n,),dtype='int32')
+    P_data = np.empty((n,),dtype=A.dtype)
+
+    #amg_core.one_point_interpolation(P_rowptr, P_colinds, A.indptr,
+    #                                 A.indices, A.data, splitting)
+    if blocksize == 1:
+        if by_val:
+            amg_core.one_point_interpolation(P_rowptr, P_colinds, P_data, A.indptr,
+                                     A.indices, A.data, splitting)
+            return csr_matrix((P_data,P_colinds,P_rowptr), shape=[n,nc])
+        else:
+            amg_core.one_point_interpolation(P_rowptr, P_colinds, P_data, C.indptr,
+                                     C.indices, C.data, splitting)
+            P_data = np.ones((n,), dtype=A.dtype)
+            return csr_matrix((P_data,P_colinds,P_rowptr), shape=[n,nc])
+    else:
+        amg_core.one_point_interpolation(P_rowptr, P_colinds, P_data, C.indptr,
+                         C.indices, C.data, splitting)
+        P_data = np.array(n*[np.identity(blocksize, dtype=A.dtype)], dtype=A.dtype)
+        return bsr_matrix((P_data,P_colinds,P_rowptr), blocksize=[blocksize,blocksize],
+                          shape=[blocksize*n,blocksize*nc])
+
+
+def neumann_AIR(A, splitting, theta=0.025, degree=1, post_theta=0):
+    """ Approximate ideal restriction using a truncated Neumann expansion for A_ff^{-1},
+    where 
+        R = [-Acf*D, I],   where
+        D = \sum_{i=0}^degree Lff^i
+
+    Parameters
+    ----------
+    A : {csr_matrix}
+        NxN matrix in CSR format
+    splitting : array
+        C/F splitting stored in an array of length N
+    theta : float : default 0.025
+        Compute approximation to ideal restriction for C, where C has rows filtered
+        with tolerance theta, that is for j s.t.
+            |C_ij| <= theta * |C_ii|        --> C_ij = 0.
+        Helps keep R sparse. 
+    degree : int in [0,4] : default 1
+        Degree of Neumann expansion. Only supported up to degree 4.
+
+    Returns
+    -------
+    Approximate ideal restriction in CSR format.
+
+    Notes
+    -----
+    Does not support block matrices.
+    """
+
+    Cpts = np.array(np.where(splitting == 1)[0], dtype='int32')
+    Fpts = np.array(np.where(splitting == 0)[0], dtype='int32')
+
+    # Convert block CF-splitting into scalar CF-splitting so that we can access
+    # submatrices of BSR matrix A
+    if isspmatrix_bsr(A):
+        bsize = A.blocksize[0]
+        Cpts *= bsize
+        Fpts *= bsize
+        Cpts0 = Cpts
+        Fpts0 = Fpts
+        for i in range(1,bsize):
+            Cpts = np.hstack([Cpts,Cpts0+i])
+            Fpts = np.hstack([Fpts,Fpts0+i])
+        Cpts.sort()
+        Fpts.sort()
+    
+    nc = Cpts.shape[0]
+    nf = Fpts.shape[0]
+    n = A.shape[0]
+    C = csr_matrix(A, copy=True)
+    if theta > 0.0:
+        filter_matrix_rows(C, theta, diagonal=True, lump=False)
+
+    # Expand sparsity pattern for R
+    C.data[np.abs(C.data)<1e-16] = 0
+    C.eliminate_zeros()
+
+    Lff = -C[Fpts,:][:,Fpts]
+    pts = np.arange(0,nf)
+    Lff[pts,pts] = 0.0
+    Lff.eliminate_zeros()
+    Acf = C[Cpts,:][:,Fpts]
+
+    # Form Neuman approximation to Aff^{-1}
+    Z = eye(nf,format='csr')
+    if degree >= 1:
+        Z += Lff
+    if degree >= 2:
+        Z += Lff*Lff
+    if degree >= 3:
+        Z += Lff*Lff*Lff
+    if degree == 4:
+        Z += Lff*Lff*Lff*Lff
+    if degree > 4:
+        raise ValueError("Only sparsity degree 0-4 supported.")
+
+    # Multiply Acf by approximation to Aff^{-1}
+    Z = -Acf*Z
+
+    if post_theta > 0.0:
+        if not isspmatrix_csr(Z):
+            Z = Z.tocsr()
+        filter_matrix_rows(Z, post_theta, diagonal=False, lump=False)
+
+    # Get sizes and permutation matrix from [F, C] block
+    # ordering to natural matrix ordering.
+    permute = eye(n,format='csr')
+    permute.indices = np.concatenate((Fpts,Cpts))
+
+    # Form R = [Z, I], reorder and return
+    R = hstack([Z, eye(nc, format='csr')])
+    if isspmatrix_bsr(A):
+        R = bsr_matrix(R * permute, blocksize=[bsize,bsize])
+    else:
+        R = csr_matrix(R * permute)
+    return R
+
+
+def local_AIR(A, splitting, theta=0.1, norm='abs', degree=1, use_gmres=False,
+                                  maxiter=10, precondition=True):
+    """ Compute approximate ideal restriction by setting RA = 0, within the
+    sparsity pattern of R. Sparsity pattern of R for the ith row (i.e. ith
+    C-point) is the set of all strongly connected F-points, or the max_row
+    *most* strongly connected F-points.
+
+    Parameters
+    ----------
+    A : {csr_matrix}
+        NxN matrix in CSR or BSR format
+    splitting : array
+        C/F splitting stored in an array of length N
+    theta : float, default 0.1
+        Solve local system for each row of R for all values
+            |A_ij| >= 0.1 * max_{i!=k} |A_ik|
+    degree : int, default 1
+        Expand sparsity pattern for R by considering strongly connected
+        neighbors within 'degree' of a given node. Only supports degree 1 and 2.
+    use_gmres : bool
+        Solve local linear system for each row of R using GMRES
+    maxiter : int
+        Maximum number of GMRES iterations
+    precondition : bool
+        Diagonally precondition GMRES
+
+    Returns
+    -------
+    Approximate ideal restriction, R, in same sparse format as A.
+
+    Notes
+    -----
+    - This was the original idea for approximating ideal restriction. In practice,
+      however, a Neumann approximation is typically used.
+    - Supports block bsr matrices as well.
+    """
+
+    # Get SOC matrix containing neighborhood to be included in local solve
+    if isspmatrix_bsr(A):
+        C = classical_strength_of_connection(A=A, theta=theta, block='amalgamate', norm=norm)
+        blocksize = A.blocksize[0]
+    elif isspmatrix_csr(A):
+        blocksize = 1
+        C = classical_strength_of_connection(A=A, theta=theta, block=None, norm=norm)
+    else:
+        try:
+            A = A.tocsr()
+            warn("Implicit conversion of A to csr", SparseEfficiencyWarning)
+            C = classical_strength_of_connection(A=A, theta=theta, block=None, norm=norm)
+            blocksize = 1
+        except:
+            raise TypeError("Invalid matrix type, must be CSR or BSR.")
+
+    Cpts = np.array(np.where(splitting == 1)[0], dtype='int32')
+    nc = Cpts.shape[0]
+    n = C.shape[0]
+
+    R_rowptr = np.empty(nc+1, dtype='int32')
+    amg_core.approx_ideal_restriction_pass1(R_rowptr, C.indptr, C.indices,
+                                            Cpts, splitting, degree)       
+
+    # Build restriction operator
+    nnz = R_rowptr[-1]
+    R_colinds = np.zeros(nnz, dtype='int32')
+
+    # Block matrix
+    if isspmatrix_bsr(A):
+        R_data = np.zeros(nnz*blocksize*blocksize, dtype=A.dtype)
+        amg_core.block_approx_ideal_restriction_pass2(R_rowptr, R_colinds, R_data, A.indptr,
+                                                      A.indices, A.data.ravel(), C.indptr,
+                                                      C.indices, C.data, Cpts, splitting,
+                                                      blocksize, degree, use_gmres, maxiter,
+                                                      precondition)
+        R = bsr_matrix((R_data.reshape(nnz,blocksize,blocksize), R_colinds, R_rowptr),
+                        blocksize=[blocksize,blocksize], shape=[nc*blocksize,A.shape[0]])
+    # Not block matrix
+    else:
+        R_data = np.zeros(nnz, dtype=A.dtype)
+        amg_core.approx_ideal_restriction_pass2(R_rowptr, R_colinds, R_data, A.indptr,
+                                                A.indices, A.data, C.indptr, C.indices,
+                                                C.data, Cpts, splitting, degree, use_gmres, maxiter,
+                                                precondition)            
+        R = csr_matrix((R_data, R_colinds, R_rowptr), shape=[nc,A.shape[0]])
+
+    R.eliminate_zeros()
+    return R
