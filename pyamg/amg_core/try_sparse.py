53874ba57f0038a1e1b7d810eb7e190f7c613b67
Luke Olson <luke.olson@gmail.com>
2019-02-04 16:48:50 -0600

    add test script

---
+++ pyamg/amg_core/try_sparse.py
@@ -0,0 +1,16 @@
+import pyamg
+import numpy as np
+import sparse
+import os
+
+n = 16
+
+A = pyamg.gallery.poisson((n,), format='csr')
+
+exact = np.zeros((n,))
+exact[0] = 1
+exact[-1] = 1
+
+result = np.zeros((n,))
+other = np.ones((n,))
+sparse.csr_matvec(n,n, A.indptr, A.indices, A.data, other, result)
