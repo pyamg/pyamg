4ac9d2c6590947aeef6c6da61bc9eca4c509c521
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-13 23:43:15 +0200

    export version tuple

--- pyamg/__init__.py
+++ pyamg/__init__.py
@@ -16,7 +16,7 @@ from .blackbox import solve, solver, solver_configuration
 
 import warnings
 
-__all__ = [__git_revision__, __version__,
+__all__ = [__version_tuple__, __version__,
            coarse_grid_solver, multilevel_solver,
            ruge_stuben_solver, smoothed_aggregation_solver, rootnode_solver,
            demo, solve, solver, solver_configuration]
