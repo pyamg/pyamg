7144e8ecb8a27d56ed42ad117a374ecdfdea7757
Nico Schlömer <nico.schloemer@gmail.com>
2021-04-14 08:35:10 +0200

    use new pybind11 for building extension modules

--- setup.py
+++ setup.py
@@ -19,13 +19,11 @@ supporting C++ code for performance critical operations.
 """
 
 import os
-import sys
 import subprocess
 
-import setuptools
-from setuptools import setup, find_packages, Extension
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, find_packages
 from setuptools.command.build_ext import build_ext
-from setuptools.command.test import test as TestCommand
 
 version = '4.1.0'
 isreleased = False
@@ -123,162 +121,27 @@ fullversion, git_revision = set_version_info(version, isreleased)
 write_version_py(version, fullversion, git_revision, isreleased,
                  filename='pyamg/version.py')
 
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        pytest.main(self.test_args)
-
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-
-def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-
-    The c++14 is prefered over c++11 (when it is available).
-    """
-    if has_flag(compiler, '-std=c++14'):
-        return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
-
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-    l_opts = {
-        'msvc': [],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        l_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        try:
-            self.compiler.compiler_so.remove("-Wstrict-prototypes")
-        except (AttributeError, ValueError):
-            pass
-        ct = self.compiler.compiler_type
-        c_opts = self.c_opts.get(ct, [])
-        l_opts = self.l_opts.get(ct, [])
-        if ct == 'unix':
-            c_opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                c_opts.append('-fvisibility=hidden')
-
-        for ext in self.extensions:
-            ext.extra_compile_args = c_opts
-            ext.extra_link_args = l_opts
-            ext.define_macros = [('VERSION_INFO', '"{}"'.format(self.distribution.get_version()))]
-
-        build_ext.build_extensions(self)
-
-    # identify extension modules
-    # since numpy is needed (for the path), need to bootstrap the setup
-    # http://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
-    def finalize_options(self):
-        build_ext.finalize_options(self)
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-        self.include_dirs.append(numpy.get_include())
-
-
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        # The issue:
-        # https://github.com/pybind/pybind11/issues/1067
-        #
-        # pybind11 will install files to
-        # TMP/pybind11-version.egg/*.h
-        # TMP/pybind11-version.egg/detail/*.h
-        #
-        # We need this to look like
-        # TMP/pybind11/*.h
-        # TMP/pybind11/detail/*.h
-
-        # TMPDIR/pybind11-2.2.4-py3.7.egg/pybind11/__init__.py
-        f = pybind11.__file__
-
-        # TMPDIR/pybind11-2.2.4-py3.7.egg/pybind11/
-        d = os.path.dirname(f)
-
-        # TMPDIR/pybind11-2.2.4-py3.7.egg
-        dd = os.path.dirname(d)
-
-        # TMPDIR
-        tmpdir = os.path.dirname(dd)
-
-        # check if not a half-install
-        if not os.path.exists(os.path.join(dd, 'pybind11.h')):
-            return pybind11.get_include(self.user)
-
-        # if it *is* a half-install
-        # Then copy all files to
-        # TMPDIR/pybind11
-        if not os.path.isdir(os.path.join(tmpdir, 'pybind11')):
-            import shutil
-            shutil.copytree(dd, os.path.join(tmpdir, 'pybind11'))
-
-        return tmpdir
-
-
-amg_core_headers = ['evolution_strength.h',
-                    'graph.h',
-                    'krylov.h',
-                    'linalg.h',
-                    'relaxation.h',
-                    'ruge_stuben.h',
-                    'smoothed_aggregation.h']
-amg_core_headers = [f.replace('.h', '') for f in amg_core_headers]
-
-ext_modules = [Extension('pyamg.amg_core.%s' % f,
-                         sources=['pyamg/amg_core/%s_bind.cpp' % f],
-                         include_dirs=[get_pybind_include(), get_pybind_include(user=True)],
-                         undef_macros=['NDEBUG'],
-                         language='c++') for f in amg_core_headers]
-
-ext_modules += [Extension('pyamg.amg_core.tests.bind_examples',
-                          sources=['pyamg/amg_core/tests/bind_examples_bind.cpp'],
-                          include_dirs=[get_pybind_include(), get_pybind_include(user=True)],
-                          language='c++')]
+# add module for each c++ file
+amg_core_headers = ['evolution_strength',
+                    'graph',
+                    'krylov',
+                    'linalg',
+                    'relaxation',
+                    'ruge_stuben',
+                    'smoothed_aggregation']
+ext_modules = [
+    Pybind11Extension(
+        f"pyamg.amg_core.{f}",
+        sources=[f"pyamg/amg_core/{f}_bind.cpp"]
+    )
+    for f in amg_core_headers
+]
+ext_modules += [
+    Pybind11Extension(
+        "pyamg.amg_core.tests.bind_examples",
+        sources=[f"pyamg/amg_core/tests/bind_examples_bind.cpp"]
+    )
+]
 
 setup(
     name='pyamg',
@@ -302,7 +165,7 @@ setup(
     zip_safe=False,
     #
     ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExt, 'test': PyTest},
+    cmdclass={"build_ext": build_ext},
     setup_requires=['numpy', 'pybind11'],
     #
     tests_require=['pytest'],
