50a60cc997ceae617c73a98b8d8b1afcde259bfb
Luke Olson <luke.olson@gmail.com>
2021-11-24 14:34:54 -0600

    split into setup.cfg

--- setup.py
+++ setup.py
@@ -21,12 +21,6 @@ supporting C++ code for performance critical operations.
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
-install_requires = (
-    'numpy>=1.7.0',
-    'scipy>=0.12.0',
-    'pytest>=2',
-)
-
 amg_core_headers = ['evolution_strength',
                     'graph',
                     'krylov',
@@ -48,55 +42,6 @@ ext_modules += [
     ]
 
 setup(
-    name='pyamg',
-    use_scm_version={
-        "version_scheme": lambda _: "4.1.0",
-        "write_to": "pyamg/version.py"
-    },
-    keywords=['algebraic multigrid AMG sparse matrix preconditioning'],
-    author='Nathan Bell, Luke Olson, and Jacob Schroder',
-    author_email='luke.olson@gmail.com',
-    maintainer='Luke Olson',
-    maintainer_email='luke.olson@gmail.com',
-    url='https://github.com/pyamg/pyamg',
-    download_url='https://github.com/pyamg/pyamg/releases',
-    license='MIT',
-    platforms=['Windows', 'Linux', 'Solaris', 'Mac OS-X', 'Unix'],
-    description=__doc__.split('\n')[0],
-    long_description=__doc__,
-    #
-    packages=find_packages(exclude=['doc']),
-    package_data={'pyamg': ['gallery/example_data/*.mat', 'gallery/mesh_data/*.npz']},
-    include_package_data=False,
-    install_requires=install_requires,
-    zip_safe=False,
-    #
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
-    setup_requires=['numpy', 'pybind11', 'setuptools_scm'],
-    #
-    tests_require=['pytest'],
-    #
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Environment :: X11 Applications',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: C++',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Education',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
 )
