d9843996c54c182f2d0d626b05bcba0288d42e76
Luke Olson <luke.olson@gmail.com>
2021-12-17 16:01:50 -0600

    finish 1dfd test
    still an outstanding ambiguous step

--- pyamg/tests/test_lloyd.py
+++ pyamg/tests/test_lloyd.py
@@ -53,7 +53,7 @@ def test_balanced_lloyd_bystep(construct_1dfd_graph):
         d[centers[a]] = 0
         m[centers[a]] = a
 
-    # Check Balanced Initialization
+    # >>Check Balanced Initialization
     assert_array_equal(d, [np.inf, 0, np.inf, np.inf, np.inf, np.inf, np.inf, 0, 0])
     assert_array_equal(m, [-1,     0,     -1,     -1,     -1,     -1,     -1, 1, 2])
 
@@ -65,7 +65,7 @@ def test_balanced_lloyd_bystep(construct_1dfd_graph):
                                              d,  m, p, pc, s,
                                              False);
 
-    # Check Pass 0 bellman_ford_balanced
+    # >>Check Pass 0 bellman_ford_balanced
     assert_array_equal(m, [0, 0, 0, 0, 1, 1, 1, 1, 2])
     assert_array_equal(d, [1, 0, 1, 2, 3, 2, 1, 0, 0])
     assert_array_equal(p, [1, -1, 1, 2, 5, 6, 7, -1, -1])
@@ -74,17 +74,51 @@ def test_balanced_lloyd_bystep(construct_1dfd_graph):
     assert changed
 
     # Pass 0 center_nodes
-    print('centers before', centers)
     changed = amg_core.center_nodes(n, Ap, Aj, Ax,
                                     Cptr,
-                                    D, P, CC, L, q,
+                                    D.ravel(), P.ravel(), CC, L, q,
                                     centers, d, m, p, s);
-    print(changed)
-    print('centers after', centers)
-    print(m)
-    print([0, 0, 0, 0, 1, 1, 1, 1, 2])
-    print(d)
-    print([1, 0, 1, 2, 3, 2, 1, 0, 0])
-    print(p)
-    print([1, -1, 1, 2, 5, 6, 7, -1, -1])
+
+    # >>Check Pass 0 center_nodes
+    assert_array_equal(centers, [1, 5, 8])
+    assert_array_equal(m, [0, 0, 0, 0, 1, 1, 1, 1, 2])
+    assert_array_equal(d, [1, 0, 1, 2, 1, 0, 1, 2, 0])
+    # tareq: assert_array_equal(p, [1, -1, 1, 2, 5, -1, 5, 6, -1])
+    assert_array_equal(p, [1, -1, 1, 2, 5, 5, 5, 6, -1])
+    # tareq: assert_array_equal(pc, [0, 2, 1, 0, 0, 2, 1, 0, 0])
+    assert_array_equal(pc, [0, 2, 1, 0, 0, 1, 1, 1, 0])
+    assert_array_equal(s, [4, 4, 1])
+    assert changed
+
+    # Pass 1 bellman_ford_balanced
+    changed = amg_core.bellman_ford_balanced(n, Ap, Aj, Ax, centers,
+                                             d,  m, p, pc, s,
+                                             False);
+
+    # >>Check Pass 1 bellman_ford_balanced
+    assert_array_equal(m, [0, 0, 0, 0, 1, 1, 1, 2, 2])
+    assert_array_equal(d, [1, 0, 1, 2, 1, 0, 1, 1, 0])
+    # tareq: assert_array_equal(p, [1, -1, 1, 2, 5, -1, 5, 8, -1])
+    assert_array_equal(p, [1, -1, 1, 2, 5, 5, 5, 8, -1])
+    # tareq: assert_array_equal(pc, [0, 2, 1, 0, 0, 2, 0, 0, 1])
+    assert_array_equal(pc, [0, 2, 1, 0, 0, 1, 0, 1, 1])
+    assert_array_equal(s, [4, 3, 2])
+    assert changed
+
+    # Pass 1 center_nodes
+    changed = amg_core.center_nodes(n, Ap, Aj, Ax,
+                                    Cptr,
+                                    D.ravel(), P.ravel(), CC, L, q,
+                                    centers, d, m, p, s);
+
+    # >>Check Pass 1 center_nodes
+    assert_array_equal(centers, [1, 5, 8])
+    assert_array_equal(m, [0, 0, 0, 0, 1, 1, 1, 2, 2])
+    assert_array_equal(d, [1, 0, 1, 2, 1, 0, 1, 1, 0])
+    # tareq: assert_array_equal(p, [1, -1, 1, 2, 5, -1, 5, 8, -1])
+    assert_array_equal(p, [1, -1, 1, 2, 5, 5, 5, 8, -1])
+    # tareq: assert_array_equal(pc, [0, 2, 1, 0, 0, 2, 0, 0, 1])
+    assert_array_equal(pc, [0, 2, 1, 0, 0, 1, 0, 1, 1])
+    assert_array_equal(s, [4, 3, 2])
+    assert not changed
 
