847d331dcb158d8aa3c51ec32e6a98f423f09f6f
bensworth <ben.s.southworth@gmail.com>
2022-12-07 08:15:39 -0700

    float type in test

--- pyamg/aggregation/tests/test_aggregate.py
+++ pyamg/aggregation/tests/test_aggregate.py
@@ -79,7 +79,7 @@ class TestAggregate(TestCase):
             assert_equal(np.setdiff1d(Cpts, expected_Cpts).shape[0], 0)
 
         # S is diagonal - no dofs aggregated
-        S = sparse.spdiags([[1, 1, 1, 1]], [0], 4, 4, format='csr')
+        S = sparse.spdiags([[1.0, 1.0, 1.0, 1.0]], [0], 4, 4, format='csr', dtype='float')
         (result, Cpts) = pairwise_aggregation(S, matchings=1, theta=0.25, norm='min')
         expected = np.eye(4)
         assert_equal(result.todense(), expected)
