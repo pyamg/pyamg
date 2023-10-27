7771983bb11c948ee4646e7f64bc97b396171059
Luke Olson <luke.olson@gmail.com>
2023-10-27 15:21:59 -0500

    build: add exension-helpers

--- setup.py
+++ setup.py
@@ -17,22 +17,55 @@ supporting C++ code for performance critical operations.
 """
 
 from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext, has_flag
+from pybind11.setup_helpers import Pybind11Extension, build_ext
 
+# extra_compile_args = []
+# extra_link_args = []
+#
+# for cflag, lflag in [('-fopenmp', '-fopenmp'),
+#                      ('-Xpreprocessor -fopenmp', '-lomp')]:
+#     try:
+#         openmp = has_flag(build_ext.compiler, cflag)
+#         if openmp:
+#             extra_compile_args.append(cflag)
+#             extra_link_args.append(lflag)
+#     except:
+#         openmp = False
+#
+# print('------------------------------------------')
+# print(extra_compile_args)
+# print(extra_link_args)
+# print('------------------------------------------')
+
+
+# OpenMP support
+from extension_helpers._openmp_helpers import get_openmp_flags, check_openmp_support
 extra_compile_args = []
 extra_link_args = []
 
-for cflag, lflag in [('-fopenmp', '-fopenmp'),
-                     ('-Xpreprocessor -fopenmp', '-lomp')] :
-    try:
-        openmp = has_flag(build_ext.compiler, cflag)
-        if openmp:
-            extra_compile_args.append(cflag)
-            extra_link_args.append(lflag)
-    except:
-        openmp = False
+# 1. try the automatic flags
+openmp_flags = get_openmp_flags()
+openmpworks = check_openmp_support(openmp_flags=openmp_flags)
+if openmpworks:
+    compile_flags = openmp_flags.get('compiler_flags')
+    link_flags = openmp_flags.get('linker_flags')
 
 print(f'\n\nUsing openmp flag {extra_compile_args}\n\n')
+# 2. try replacing -fopenmp with mac specific flags
+if not openmpworks:
+    compile_flags = openmp_flags.get('compiler_flags')
+    link_flags = openmp_flags.get('linker_flags')
+    for i, flag in enumerate(compile_flags):
+        if flag == '-fopenmp':
+            compile_flags[i] = '-Xpreprocessor -fopenmp'
+    for i, flag in enumerate(link_flags):
+        if flag == '-fopenmp':
+            link_flags[i] = '-lomp'
+    openmpworks = check_openmp_support(openmp_flags=openmp_flags)
+
+if openmpworks:
+    extra_compile_args = compile_flags
+    extra_link_args = link_flags
 
 amg_core_headers = ['air',
                     'evolution_strength',
